23c440573bf61fe4b49aeec33a006737f1a25f85
Ufuk <ufukty@gmail.com>
2022-11-26 22:25:50 +0300

    testing

--- testing/testing.go
+++ testing/testing.go
@@ -1,13 +1,9 @@
 package testing
 
 type Run struct {
-	expected
+	totalChecks       int
+	totalFailedChecks int
 }
-
-const (
-	HitOrMiss           = iota
-	MeasurableErrorRate = iota
-)
 
 type Objective struct {
 	Tag  string
@@ -16,22 +12,19 @@ type Objective struct {
 }
 
 func (r *Run) Assert(left, right int) {
-	r.TotalCalls += 1
-	if o.Kind == HitOrMiss {
-		//
-	}
+	r.totalChecks += 1
+	
 }
 
-func (o *Objective) NewRun(test func(run *Run)) {
-	run := Run{
-		Tag:  tag,
-		Kind: kind,
-	}
+func (o *Objective) NewRun(test func(r *Run)) {
+	run := Run{0, 0}
 	o.Runs = append(o.Runs, run)
 	test(&run)
 }
 
-// type TestFunction func(t *Testing)
+type Candidate struct {
+	
+}
 
 type Testing struct {
 	Objectives []Objective
@@ -41,18 +34,15 @@ func CreateTesting() *Testing {
 	return &Testing{}
 }
 
-func (t *Testing) NewObjective(tag string, isErrorRateMeasurable bool, test func(objective *Objective)) {
-	var kind int
-	if isErrorRateMeasurable {
-		kind = MeasurableErrorRate
-	} else {
-		kind = HitOrMiss
-	}
-
+func (t *Testing) NewObjective(tag string, test func(o *Objective)) {
 	objective := Objective{
-		Tag:  tag,
-		Kind: kind,
+		Tag: tag,
 	}
 	t.Objectives = append(t.Objectives, objective)
 	test(&objective)
 }
+
+
+func (t *Testing) Candidate(candidateTesting func(c *Candidate) ) {
+	for id, ind := range t.
+})
