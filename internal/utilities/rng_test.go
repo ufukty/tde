18f293de65ccea96329c00f63ba3269a1deda5c8
Ufuk <ufukty@gmail.com>
2023-01-04 13:50:02 +0300

    utilities/Pick test

--- internal/utilities/rng_test.go
+++ internal/utilities/rng_test.go
@@ -1,6 +1,7 @@
 package utilities
 
 import (
+	"fmt"
 	"log"
 	"math"
 	"testing"
@@ -34,4 +35,18 @@ func Test_URandFloatForCrypto(t *testing.T) {
 		mostFrequentRangePerRun[IndexOfMax(frequencies)]++
 	}
 	log.Println("mostFrequentRangePerRun:", mostFrequentRangePerRun)
+}
+
+func Test_Pick(t *testing.T) {
+	array := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	freq := []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	for i := 0; i < 10000; i++ {
+		freq[*Pick(array)]++
+	}
+	for i, fr := range freq {
+		if fr == 0 {
+			t.Errorf("TestPick didn't returned any number of %dth item.", i)
+		}
+	}
+	fmt.Println(freq)
 }
