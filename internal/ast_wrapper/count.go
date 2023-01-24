6ad15298d9dd0efabb3259d58da4b13b4f949074
Ufuk <ufukty@gmail.com>
2023-01-24 20:59:25 +0300

    ast_wrapper:CountNonNilNodes

---
+++ internal/ast_wrapper/count.go
@@ -0,0 +1,14 @@
+package ast_wrapper
+
+import "go/ast"
+
+func CountNonNilNodes(node ast.Node) int {
+	counter := 0
+	Traverse(GetTraversableNodeForASTNode(node), func(tNodePtr *TraversableNode) bool {
+		if node != nil {
+			counter++
+		}
+		return true
+	})
+	return counter
+}
