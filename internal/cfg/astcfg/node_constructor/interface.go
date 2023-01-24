43ee39dda5a45030eaf2fbceb00283bbea48009c
Ufuk <ufukty@gmail.com>
2023-01-24 21:00:44 +0300

    node_constructor:Fix limit decrement twice on Interface type constructors

--- internal/cfg/astcfg/node_constructor/interface.go
+++ internal/cfg/astcfg/node_constructor/interface.go
@@ -9,40 +9,25 @@ import (
 
 // Chooses a node type that confirms ast.Spec interface; initializes and returns
 func Spec(ctx context.Context, limit int) ast.Spec {
-	if limit == 0 {
-		return nil
-	}
-	return (*utilities.Pick(SpecificationConstructors))(ctx, limit-1)
+	return (*utilities.Pick(SpecificationConstructors))(ctx, limit)
 }
 
 // Chooses a node type that confirms ast.Decl interface; initializes and returns
 func Decl(ctx context.Context, limit int) ast.Decl {
-	if limit == 0 {
-		return nil
-	}
-	return (*utilities.Pick(DeclarationConstructors))(ctx, limit-1)
+	return (*utilities.Pick(DeclarationConstructors))(ctx, limit)
 }
 
 // Chooses a node type that confirms ast.Expr interface; initializes and returns
 func Expr(ctx context.Context, limit int) ast.Expr {
-	if limit == 0 {
-		return nil
-	}
-	return (*utilities.Pick(ExpressionConstructors))(ctx, limit-1)
+	return (*utilities.Pick(ExpressionConstructors))(ctx, limit)
 }
 
 // Chooses a node type that confirms ast.Stmt interface; initializes and returns
 func Stmt(ctx context.Context, limit int) ast.Stmt {
-	if limit == 0 {
-		return nil
-	}
-	return (*utilities.Pick(StatementConstructors))(ctx, limit-1)
+	return (*utilities.Pick(StatementConstructors))(ctx, limit)
 }
 
 // Chooses, initialized and returns an instance of random Type Declaration (Expression)
 func Type(ctx context.Context, limit int) ast.Expr {
-	if limit == 0 {
-		return nil
-	}
-	return (*utilities.Pick(TypeDeclarationConstructors))(ctx, limit-1)
+	return (*utilities.Pick(TypeDeclarationConstructors))(ctx, limit)
 }
