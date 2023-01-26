d3a7ce569c018ad3160b74669251278cdb836b63
Ufuk <ufukty@gmail.com>
2023-01-26 22:09:52 +0300

    cfg/context

---
+++ internal/cfg/context/examine.go
@@ -0,0 +1,70 @@
+package context
+
+import (
+	"go/ast"
+	"go/token"
+)
+
+func ExamineEnteringNode(ctx *Context, node ast.Node) {
+	switch node := node.(type) {
+	case
+		*ast.BlockStmt,
+		*ast.FuncDecl,
+		*ast.ForStmt,
+		*ast.IfStmt,
+		*ast.SwitchStmt,
+		*ast.TypeSwitchStmt:
+		ctx.ScopeIn()
+
+	case
+		*ast.Package:
+		for _, file := range node.Files {
+			for _, decl := range file.Decls {
+				if funcDecl, ok := decl.(*ast.FuncDecl); ok {
+					ctx.AddVariable(*funcDecl.Name)
+				}
+			}
+		}
+
+	case
+		*ast.File:
+		for _, decl := range node.Decls {
+			if funcDecl, ok := decl.(*ast.FuncDecl); ok {
+				ctx.AddVariable(*funcDecl.Name)
+			}
+		}
+	}
+}
+
+func ExamineLeavingNode(ctx *Context, node ast.Node) {
+	switch node := node.(type) {
+	case
+		*ast.BlockStmt,
+		*ast.FuncDecl,
+		*ast.ForStmt,
+		*ast.IfStmt,
+		*ast.SwitchStmt,
+		*ast.TypeSwitchStmt:
+		ctx.ScopeOut()
+
+	case
+		*ast.AssignStmt:
+		if node.Tok == token.DEFINE {
+			for _, expr := range node.Lhs {
+				if ident, ok := expr.(*ast.Ident); ok {
+					ctx.AddVariable(*ident)
+				}
+			}
+		}
+
+	case
+		*ast.FuncType:
+		if fieldList := node.Params.List; fieldList != nil {
+			for _, field := range fieldList {
+				for _, ident := range field.Names {
+					ctx.AddVariable(*ident)
+				}
+			}
+		}
+	}
+}
--- internal/cfg/context/inspect.go
+++ internal/cfg/context/inspect.go
@@ -2,80 +2,20 @@ package context
 
 import (
 	"go/ast"
-	"go/token"
 	"tde/internal/astw"
 )
 
 // TODO: Detect FuncLit's in code and add to Context
+// FIXME: convert to BFS
 func InspectWithContext(startNode ast.Node, callback func(ctx Context, node ast.Node)) {
 	ctx := NewContext()
 	astw.InspectTwiceWithTrace(startNode,
-		// before visit children
-		func(currentNode ast.Node, parentTrace []ast.Node, childIndexTrace []int) bool {
-			switch currentNode := currentNode.(type) {
-			case
-				*ast.BlockStmt,
-				*ast.FuncDecl,
-				*ast.ForStmt,
-				*ast.IfStmt,
-				*ast.SwitchStmt,
-				*ast.TypeSwitchStmt:
-				ctx.ScopeIn()
-
-			case
-				*ast.Package:
-				for _, file := range currentNode.Files {
-					for _, decl := range file.Decls {
-						if funcDecl, ok := decl.(*ast.FuncDecl); ok {
-							ctx.AddVariable(*funcDecl.Name)
-						}
-					}
-				}
-
-			case
-				*ast.File:
-				for _, decl := range currentNode.Decls {
-					if funcDecl, ok := decl.(*ast.FuncDecl); ok {
-						ctx.AddVariable(*funcDecl.Name)
-					}
-				}
-			}
-
-			callback(ctx, currentNode)
+		func(node ast.Node, parents []ast.Node, indices []int) bool {
+			ExamineEnteringNode(&ctx, node)
+			callback(ctx, node)
 			return true
+		}, func(node ast.Node, parents []ast.Node, indices []int) {
+			ExamineLeavingNode(&ctx, node)
 		},
-		// after visit children
-		func(currentNode ast.Node, parentTrace []ast.Node, childIndexTrace []int) {
-			switch node := currentNode.(type) {
-			case
-				*ast.BlockStmt,
-				*ast.FuncDecl,
-				*ast.ForStmt,
-				*ast.IfStmt,
-				*ast.SwitchStmt,
-				*ast.TypeSwitchStmt:
-				ctx.ScopeOut()
-
-			case
-				*ast.AssignStmt:
-				if node.Tok == token.DEFINE {
-					for _, expr := range node.Lhs {
-						if ident, ok := expr.(*ast.Ident); ok {
-							ctx.AddVariable(*ident)
-						}
-					}
-				}
-
-			case
-				*ast.FuncType:
-				if fieldList := node.Params.List; fieldList != nil {
-					for _, field := range fieldList {
-						for _, ident := range field.Names {
-							ctx.AddVariable(*ident)
-						}
-					}
-				}
-			}
-		})
-
+	)
 }
