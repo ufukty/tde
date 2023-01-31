a5a9a7d4ccf9b6748b00d3bc18275dd04c3cc4eb
Ufuk <ufukty@gmail.com>
2023-01-31 11:40:32 +0300

    utilities:Min, Max for integers

---
+++ internal/utilities/int.go
@@ -0,0 +1,17 @@
+package utilities
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func Max(a, b int) int {
+	if a < b {
+		return b
+	} else {
+		return a
+	}
+}
