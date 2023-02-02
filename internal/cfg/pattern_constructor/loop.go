086b04b403e1a9b8cbce9dd58bf08083a8855b25
Ufuk <ufukty@gmail.com>
2023-02-02 15:36:34 +0300

    pattern_constructor: initial

---
+++ internal/cfg/pattern_constructor/loop.go
@@ -0,0 +1,58 @@
+package pattern_constructor
+
+import (
+	"go/ast"
+	"go/token"
+	"tde/internal/cfg/context"
+	"tde/internal/cfg/node_constructor"
+)
+
+func LoopOverSlice(ctx *context.Context, limit int, slice *ast.Ident) *ast.RangeStmt {
+	if limit == 0 {
+		return nil
+	}
+	return &ast.RangeStmt{
+		Key:   nil,
+		Value: nil,
+		X:     nil,
+		Body:  node_constructor.BlockStmt(ctx, limit-1),
+	}
+}
+
+//	for i := 0; i < min(len(a), len(b)); i++ {
+//	  a_, b_ := a[i], b[i]
+//	  ...
+//	}
+func LoopOverSlices(ctx *context.Context, limit int, sliceA, sliceB *ast.Ident) *ast.ForStmt {
+	if limit == 0 {
+		return nil
+	}
+	return &ast.ForStmt{
+		Init: nil,
+		Cond: &ast.BinaryExpr{
+			
+		},
+		Post: nil,
+		Body: &ast.BlockStmt{
+			List: []ast.Stmt{
+				&ast.AssignStmt{
+					Tok: token.DEFINE,
+					Lhs: []ast.Expr{
+						&ast.Ident{Name: "valueA"},
+						&ast.Ident{Name: "valueB"},
+					},
+					Rhs: []ast.Expr{
+						&ast.IndexExpr{
+							X:     sliceA,
+							Index: &ast.Ident{Name: "i"},
+						},
+						&ast.IndexExpr{
+							X:     sliceB,
+							Index: &ast.Ident{Name: "i"},
+						},
+					},
+				},
+			},
+		},
+	}
+}
