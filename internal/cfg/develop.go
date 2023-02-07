5b9660f5de95a50f9d0c4ae43fef5b6911acfedf
Ufuk <ufukty@gmail.com>
2023-02-07 20:03:17 +0300

    cfg/Develop add a parameter and improve tests

--- internal/cfg/develop.go
+++ internal/cfg/develop.go
@@ -1,8 +1,6 @@
 package cfg
 
 import (
-	"fmt"
-	"reflect"
 	trav "tde/internal/astw/traverse"
 	ast_types "tde/internal/astw/types"
 	"tde/internal/cfg/context_resolution"
@@ -25,284 +23,162 @@ func listAppendableSpots(node ast.Node) (appandableNodes []*trav.TraversableNode
 	return
 }
 
-func appendRandomly(tNode *trav.TraversableNode, ctx *context.Context) {
+func appendRandomly(tNode *trav.TraversableNode, ctx *context.Context, depthLimit int) {
 
 	if tNode.ExpectedType.IsSliceType() {
 		switch tNode.Value.(type) {
 		case []*ast.ImportSpec:
-			createdNode := nc.ImportSpec(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.ImportSpec(ctx, depthLimit))
 		case []*ast.Ident:
-			createdNode := nc.Ident(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.Ident(ctx, depthLimit))
 		case []*ast.Field:
-			createdNode := nc.Field(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.Field(ctx, depthLimit))
 		case []ast.Stmt:
-			createdNode := nc.Stmt(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.Stmt(ctx, depthLimit))
 		case []ast.Decl:
-			createdNode := nc.Decl(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.Decl(ctx, depthLimit))
 		case []ast.Spec:
-			createdNode := nc.Spec(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.Spec(ctx, depthLimit))
 		case []ast.Expr:
-			createdNode := nc.Expr(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.Expr(ctx, depthLimit))
 		}
 
 	} else if tNode.ExpectedType.IsInterfaceType() {
 		switch tNode.ExpectedType {
 		case ast_types.Expr:
-			createdNode := nc.Expr(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.Expr(ctx, depthLimit))
 		case ast_types.Stmt:
-			createdNode := nc.Stmt(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.Stmt(ctx, depthLimit))
 		case ast_types.Decl:
-			createdNode := nc.Decl(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.Decl(ctx, depthLimit))
 		case ast_types.Spec:
-			createdNode := nc.Spec(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.Spec(ctx, depthLimit))
 		case ast_types.TypeExpr:
-			createdNode := nc.Type(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.Type(ctx, depthLimit))
 		}
 
 	} else if tNode.ExpectedType.IsConcreteType() {
 
 		switch tNode.ExpectedType {
 		// case ast_types.BadDecl:
-		// createdNode:=nc.BadDecl(ctx, 1)
+		// createdNode:=nc.BadDecl(ctx, depthLimit)
 		// 	tNode.Ref.Set(createdNode)
 		// case ast_types.BadExpr:
-		// createdNode:=nc.BadExpr(ctx, 1)
+		// createdNode:=nc.BadExpr(ctx, depthLimit)
 		// 	tNode.Ref.Set(createdNode)
 		// case ast_types.BadStmt:
-		// createdNode:=nc.BadStmt(ctx, 1)
+		// createdNode:=nc.BadStmt(ctx, depthLimit)
 		// 	tNode.Ref.Set(createdNode)
 		// case ast_types.Comment:
-		// createdNode:=nc.Comment(ctx, 1)
+		// createdNode:=nc.Comment(ctx, depthLimit)
 		// 	tNode.Ref.Set(createdNode)
 		// case ast_types.CommentGroup:
-		// createdNode:=nc.CommentGroup(ctx, 1)
+		// createdNode:=nc.CommentGroup(ctx, depthLimit)
 		// 	tNode.Ref.Set(createdNode)
 		// case ast_types.File:
-		// createdNode:=nc.File(ctx, 1)
+		// createdNode:=nc.File(ctx, depthLimit)
 		// 	tNode.Ref.Set(createdNode)
 		// case ast_types.Package:
-		// createdNode:=nc.Package(ctx, 1)
+		// createdNode:=nc.Package(ctx, depthLimit)
 		// 	tNode.Ref.Set(createdNode)
 		case ast_types.ArrayType:
-			createdNode := nc.ArrayType(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.ArrayType(ctx, depthLimit))
 		case ast_types.AssignStmt:
-			createdNode := nc.AssignStmt(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.AssignStmt(ctx, depthLimit))
 		case ast_types.BasicLit:
-			createdNode := nc.BasicLit(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.BasicLit(ctx, depthLimit))
 		case ast_types.BinaryExpr:
-			createdNode := nc.BinaryExpr(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.BinaryExpr(ctx, depthLimit))
 		case ast_types.BlockStmt:
-			createdNode := nc.BlockStmt(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.BlockStmt(ctx, depthLimit))
 		case ast_types.BranchStmt:
-			createdNode := nc.BranchStmt(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.BranchStmt(ctx, depthLimit))
 		case ast_types.CallExpr:
-			createdNode := nc.CallExpr(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.CallExpr(ctx, depthLimit))
 		case ast_types.CaseClause:
-			createdNode := nc.CaseClause(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.CaseClause(ctx, depthLimit))
 		case ast_types.ChanType:
-			createdNode := nc.ChanType(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.ChanType(ctx, depthLimit))
 		case ast_types.CommClause:
-			createdNode := nc.CommClause(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.CommClause(ctx, depthLimit))
 		case ast_types.CompositeLit:
-			createdNode := nc.CompositeLit(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.CompositeLit(ctx, depthLimit))
 		case ast_types.DeclStmt:
-			createdNode := nc.DeclStmt(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.DeclStmt(ctx, depthLimit))
 		case ast_types.DeferStmt:
-			createdNode := nc.DeferStmt(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.DeferStmt(ctx, depthLimit))
 		case ast_types.Ellipsis:
-			createdNode := nc.Ellipsis(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.Ellipsis(ctx, depthLimit))
 		case ast_types.EmptyStmt:
-			createdNode := nc.EmptyStmt(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.EmptyStmt(ctx, depthLimit))
 		case ast_types.ExprStmt:
-			createdNode := nc.ExprStmt(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.ExprStmt(ctx, depthLimit))
 		case ast_types.Field:
-			createdNode := nc.Field(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.Field(ctx, depthLimit))
 		case ast_types.FieldList:
-			createdNode := nc.FieldList(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.FieldList(ctx, depthLimit))
 		case ast_types.ForStmt:
-			createdNode := nc.ForStmt(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.ForStmt(ctx, depthLimit))
 		case ast_types.FuncDecl:
-			createdNode := nc.FuncDecl(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.FuncDecl(ctx, depthLimit))
 		case ast_types.FuncLit:
-			createdNode := nc.FuncLit(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.FuncLit(ctx, depthLimit))
 		case ast_types.FuncType:
-			createdNode := nc.FuncType(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.FuncType(ctx, depthLimit))
 		case ast_types.GenDecl:
-			createdNode := nc.GenDecl(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.GenDecl(ctx, depthLimit))
 		case ast_types.GoStmt:
-			createdNode := nc.GoStmt(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.GoStmt(ctx, depthLimit))
 		case ast_types.Ident:
-			createdNode := nc.Ident(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.Ident(ctx, depthLimit))
 		case ast_types.IfStmt:
-			createdNode := nc.IfStmt(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.IfStmt(ctx, depthLimit))
 		case ast_types.ImportSpec:
-			createdNode := nc.ImportSpec(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.ImportSpec(ctx, depthLimit))
 		case ast_types.IncDecStmt:
-			createdNode := nc.IncDecStmt(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.IncDecStmt(ctx, depthLimit))
 		case ast_types.IndexExpr:
-			createdNode := nc.IndexExpr(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.IndexExpr(ctx, depthLimit))
 		case ast_types.IndexListExpr:
-			createdNode := nc.IndexListExpr(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.IndexListExpr(ctx, depthLimit))
 		case ast_types.InterfaceType:
-			createdNode := nc.InterfaceType(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.InterfaceType(ctx, depthLimit))
 		case ast_types.KeyValueExpr:
-			createdNode := nc.KeyValueExpr(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.KeyValueExpr(ctx, depthLimit))
 		case ast_types.LabeledStmt:
-			createdNode := nc.LabeledStmt(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.LabeledStmt(ctx, depthLimit))
 		case ast_types.MapType:
-			createdNode := nc.MapType(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.MapType(ctx, depthLimit))
 		case ast_types.ParenExpr:
-			createdNode := nc.ParenExpr(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.ParenExpr(ctx, depthLimit))
 		case ast_types.RangeStmt:
-			createdNode := nc.RangeStmt(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.RangeStmt(ctx, depthLimit))
 		case ast_types.ReturnStmt:
-			createdNode := nc.ReturnStmt(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.ReturnStmt(ctx, depthLimit))
 		case ast_types.SelectorExpr:
-			createdNode := nc.SelectorExpr(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.SelectorExpr(ctx, depthLimit))
 		case ast_types.SelectStmt:
-			createdNode := nc.SelectStmt(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.SelectStmt(ctx, depthLimit))
 		case ast_types.SendStmt:
-			createdNode := nc.SendStmt(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.SendStmt(ctx, depthLimit))
 		case ast_types.SliceExpr:
-			createdNode := nc.SliceExpr(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.SliceExpr(ctx, depthLimit))
 		case ast_types.StarExpr:
-			createdNode := nc.StarExpr(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.StarExpr(ctx, depthLimit))
 		case ast_types.StructType:
-			createdNode := nc.StructType(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.StructType(ctx, depthLimit))
 		case ast_types.SwitchStmt:
-			createdNode := nc.SwitchStmt(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.SwitchStmt(ctx, depthLimit))
 		case ast_types.TypeAssertExpr:
-			createdNode := nc.TypeAssertExpr(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.TypeAssertExpr(ctx, depthLimit))
 		case ast_types.TypeSpec:
-			createdNode := nc.TypeSpec(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.TypeSpec(ctx, depthLimit))
 		case ast_types.TypeSwitchStmt:
-			createdNode := nc.TypeSwitchStmt(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.TypeSwitchStmt(ctx, depthLimit))
 		case ast_types.UnaryExpr:
-			createdNode := nc.UnaryExpr(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.UnaryExpr(ctx, depthLimit))
 		case ast_types.ValueSpec:
-			createdNode := nc.ValueSpec(ctx, 1)
-			fmt.Println(reflect.ValueOf(createdNode))
-			tNode.Ref.Set(createdNode)
+			tNode.Ref.Set(nc.ValueSpec(ctx, depthLimit))
 		}
 
 	}
@@ -310,7 +186,7 @@ func appendRandomly(tNode *trav.TraversableNode, ctx *context.Context) {
 
 // Picks an appandable spot randomly (either a nil field or end of a slice type field)
 // Creates just one node and appends to choosen spot
-func Develop(astPkg *ast.Package, astFile *ast.File, astFuncDecl *ast.FuncDecl) error {
+func Develop(astPkg *ast.Package, astFile *ast.File, astFuncDecl *ast.FuncDecl, depthLimit int) error {
 	availableSpots := listAppendableSpots(astFuncDecl.Body)
 	if len(availableSpots) == 0 {
 		return errors.New("No available spots found in AST to place new node")
@@ -324,6 +200,6 @@ func Develop(astPkg *ast.Package, astFile *ast.File, astFuncDecl *ast.FuncDecl) 
 	if err != nil {
 		return errors.Wrap(err, "Failed")
 	}
-	appendRandomly(choosenSpot, ctx)
+	appendRandomly(choosenSpot, ctx, depthLimit)
 	return nil
 }
--- internal/cfg/develop_test.go
+++ internal/cfg/develop_test.go
@@ -4,7 +4,6 @@ import (
 	"tde/internal/astw/clone"
 	ast_utl "tde/internal/astw/utilities"
 
-	"fmt"
 	"testing"
 
 	"github.com/pkg/errors"
@@ -19,17 +18,34 @@ func Test_Develop(t *testing.T) {
 	astFile := astPkg.Files["../test_package/walk.go"]
 	originalFuncDecl, _ := ast_utl.FindFuncDecl(astPkg, "WalkWithNils")
 
-	for i := 0; i < 10; i++ {
-		t.Run(fmt.Sprintf("Run: %d", i), func(t *testing.T) {
-			candidateFuncDecl := clone.FuncDecl(originalFuncDecl)
-			err := Develop(astPkg, astFile, candidateFuncDecl)
-			if err != nil {
-				t.Error(errors.Wrapf(err, "Failed on Develop"))
-			}
-			if ast_utl.CompareRecursively(candidateFuncDecl, originalFuncDecl) == true {
-				t.Errorf("Failed to see change on candidate #%d\n", i)
-			}
-		})
+	candidateFuncDecl := clone.FuncDecl(originalFuncDecl)
+	err = Develop(astPkg, astFile, candidateFuncDecl, 1)
+	if err != nil {
+		t.Error(errors.Wrapf(err, "Failed on Develop"))
+	}
+	if ast_utl.CompareRecursively(candidateFuncDecl, originalFuncDecl) == true {
+		t.Error("Failed to see change on candidate")
+	}
+}
+
+func Benchmark_Develop(b *testing.B) {
+	_, astPkgs, err := ast_utl.LoadDir("../test_package")
+	if err != nil {
+		b.Error(errors.Wrapf(err, "Failed test prep"))
+	}
+	astPkg := astPkgs["test_package"]
+	astFile := astPkg.Files["../test_package/walk.go"]
+	originalFuncDecl, _ := ast_utl.FindFuncDecl(astPkg, "WalkWithNils")
+
+	for i := 0; i < b.N; i++ {
+		candidateFuncDecl := clone.FuncDecl(originalFuncDecl)
+		err := Develop(astPkg, astFile, candidateFuncDecl, 1)
+		if err != nil {
+			b.Error(errors.Wrapf(err, "Failed on Develop"))
+		}
+		if ast_utl.CompareRecursively(candidateFuncDecl, originalFuncDecl) == true {
+			b.Errorf("Failed to see change on candidate #%d\n", i)
+		}
 	}
 }
 
@@ -43,19 +59,9 @@ func Test_SequentialDevelop(t *testing.T) {
 	funcDecl, _ := ast_utl.FindFuncDecl(astPkg, "WalkWithNils")
 
 	for i := 0; i < 2000; i++ {
-		err := Develop(astPkg, astFile, funcDecl)
+		err := Develop(astPkg, astFile, funcDecl, 1)
 		if err != nil {
 			t.Error(errors.Wrapf(err, "Failed on Develop"))
 		}
 	}
-}
-
-func Test_DevelopFindUnbreakingChange(t *testing.T) {
-	// _, astPkgs, err := ast_utl.LoadDir("../test_package")
-	// if err != nil {
-	// 	t.Error(errors.Wrapf(err, "Failed test prep"))
-	// }
-	// astPkg := astPkgs["test_package"]
-	// astFile := astPkg.Files["../test_package/walk.go"]
-	// originalFuncDecl, _ := ast_utl.FindFuncDecl(astPkg, "WalkWithNils")
 }
