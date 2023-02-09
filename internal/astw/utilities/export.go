114b9b8e8aa68106525bda9ce126cfca0b404739
Ufuk <ufukty@gmail.com>
2023-02-09 23:24:20 +0300

    astw/utilities:Diff

--- internal/astw/utilities/export.go
+++ internal/astw/utilities/export.go
@@ -1,11 +1,14 @@
 package utilities
 
 import (
+	"bytes"
 	"go/ast"
 	"go/printer"
 	"go/token"
+	"strings"
 	"tde/internal/utilities"
 
+	"github.com/kylelemons/godebug/diff"
 	"github.com/pkg/errors"
 )
 
@@ -17,4 +20,21 @@ func String(node ast.Node) (string, error) {
 		return "", errors.Wrapf(err, "failed print")
 	}
 	return sw.String(), nil
+}
+
+func Diff(current, new ast.Node) string {
+	bufferCurrent := bytes.NewBuffer([]byte{})
+	ast.Fprint(bufferCurrent, token.NewFileSet(), current, nil)
+
+	bufferNew := bytes.NewBuffer([]byte{})
+	ast.Fprint(bufferNew, token.NewFileSet(), current, nil)
+
+	diffStr := diff.Diff(bufferCurrent.String(), bufferNew.String())
+	changedLines := ""
+	for _, str := range strings.Split(diffStr, "\n") {
+		if strings.Index(str, " ") != 0 {
+			changedLines = changedLines + str
+		}
+	}
+	return changedLines
 }
