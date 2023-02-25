eac26e983a74595971e9950d6bb27479b3c40b18
Ufuk <ufukty@gmail.com>
2023-02-25 22:19:50 +0300

    cmd/evolver-server: rename + adds upload handler

---
+++ cmd/evolver-server/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"tde/internal/evolution"
+	"tde/internal/server"
+)
+
+func GetParametersForUser() {}
+
+func StartEvolution(input string, test string, populationSize int) { // FIXME: actual input is a package, contains tests and target function headers along with irrelevant code
+	e := evolution.Evolution{}
+	e.InitPopulation(populationSize)
+	e.IterateLoop(context.Background())
+}
+
+func main() {
+
+	server.NewServer(6000)
+
+	http.HandleFunc("/upload", uploadFile)
+	http.ListenAndServe(":8080", nil)
+
+	// t := Testing{}
+
+	// provisionEnvironmentImage [x]
+	// run tests
+
+	// fitness := t.Calculate()
+	// fmt.Println(fitness)
+
+}
