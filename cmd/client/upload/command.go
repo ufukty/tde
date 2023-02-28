4ce08b0d076c1f01de48bee1715e948b7b7843f5
Ufuk <ufukty@gmail.com>
2023-02-28 22:41:04 +0300

    cmd/client/upload command flags

--- cmd/client/upload/command.go
+++ cmd/client/upload/command.go
@@ -17,6 +17,7 @@ func (c *Command) BindFlagsToArgs(f *flag.FlagSet) {}
 
 func (c *Command) ManualParser(f *flag.FlagSet) error {
 	f.Var(&c.ExcludeDirs, "exclude-dir", "")
+	f.Var(&c.ExcludeDirs, "e", "")
 
 	err := f.Parse(os.Args[2:])
 	if err != nil {
