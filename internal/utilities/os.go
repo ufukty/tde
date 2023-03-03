d2985e3018f3d0835a5cc09b9c8690bffc0bf4a2
Ufuk <ufukty@gmail.com>
2023-03-03 13:18:13 +0300

    utilities/os:WorkingDir

--- internal/utilities/os.go
+++ internal/utilities/os.go
@@ -3,9 +3,19 @@ package utilities
 import (
 	"fmt"
 	"os"
+
+	"github.com/pkg/errors"
 )
 
 func Terminate(msg ...any) {
 	fmt.Println(msg...)
 	os.Exit(1)
+}
+
+func WorkingDir() (string, error) {
+	stdOut, _, err := RunCommandForOutput("pwd", "-P")
+	if err != nil {
+		return "", errors.Wrap(err, "Failed to run command pwd")
+	}
+	return stdOut, nil
 }
