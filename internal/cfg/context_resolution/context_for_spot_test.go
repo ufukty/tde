5e1149d699bf6d5b1f2a4eb76c5da7e4bd56ae4e
Ufuk <ufukty@gmail.com>
2023-03-09 15:49:27 +0300

    cfg/context_resolution enhance test

--- internal/cfg/context_resolution/context_for_spot_test.go
+++ internal/cfg/context_resolution/context_for_spot_test.go
@@ -1,22 +1,25 @@
 package context_resolution
 
 import (
-	trav "tde/internal/astw/traverse"
-	utl "tde/internal/astw/utilities"
-
 	"fmt"
+	"go/ast"
+	trav "tde/internal/astw/traverse"
+	ast_utl "tde/internal/astw/utilities"
+	"tde/internal/utilities"
+
 	"testing"
 
 	"github.com/pkg/errors"
+	"golang.org/x/exp/slices"
 )
 
 func Test_GetContextForSpot(t *testing.T) {
-	_, astPkgs, err := utl.LoadDir("../../test_package")
+	_, astPkgs, err := ast_utl.LoadDir("../../test_package")
 	if err != nil {
 		t.Error(errors.Wrapf(err, "Failed on loading package"))
 	}
 	astPkg := astPkgs["test_package"]
-	funcDecl, err := utl.FindFuncDecl(astPkg, "WalkWithNils")
+	funcDecl, err := ast_utl.FindFuncDecl(astPkg, "WalkWithNils")
 	if err != nil {
 		t.Error(errors.Wrapf(err, "Failed on preparation"))
 	}
@@ -34,5 +37,35 @@ func Test_GetContextForSpot(t *testing.T) {
 		t.Error(errors.Wrapf(err, ""))
 	}
 
-	fmt.Println("Context:\n", ctx)
+	vars := utilities.Map(ctx.Scopes[1].Variables, func(i int, n *ast.Ident) string {
+		return n.Name
+	})
+	if !slices.Contains(vars, "root") {
+		t.Error("validation: variable 'root' not found in context")
+	}
+	if !slices.Contains(vars, "callback") {
+		t.Error("validation: variable 'callback' not found in context")
+	}
+
+	for i, scope := range ctx.Scopes {
+		fmt.Println("scope:", i)
+		for _, variable := range scope.Variables {
+			fmt.Println("variable:", variable.Name)
+		}
+		for _, function := range scope.Functions {
+			fmt.Println("function:", function.Name)
+		}
+		for _, import_ := range scope.Imports {
+			fmt.Println("import:", import_.Path.Value)
+		}
+		for _, type_ := range scope.Types {
+			fmt.Println("types:", type_.Name)
+		}
+		for _, method := range scope.Methods {
+			fmt.Println("method:", method.Recv.List[0].Type, method.Name)
+		}
+		fmt.Println()
+	}
+
+	// fmt.Println("Context:\n", ctx)
 }
