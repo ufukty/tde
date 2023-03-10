436bcc4091cf8fcc0ab9afb12f669f30dee315fc
Ufuk <ufukty@gmail.com>
2023-03-10 12:57:06 +0300

    genetics/mutation/token_shuffle

---
+++ internal/genetics/mutation/token_shuffle/token_shuffle.go
@@ -0,0 +1,68 @@
+package token_shuffle
+
+import (
+	"go/ast"
+	"go/token"
+	"tde/internal/tokenw"
+	utl "tde/internal/utilities"
+)
+
+func listTokenContainingNodes(n ast.Node) (tokenContainingNodes []ast.Node) {
+	ast.Inspect(n, func(n ast.Node) bool {
+		switch n := n.(type) {
+		case
+			*ast.BasicLit,
+			*ast.UnaryExpr,
+			*ast.BinaryExpr,
+			*ast.IncDecStmt,
+			*ast.AssignStmt,
+			*ast.BranchStmt,
+			*ast.RangeStmt,
+			*ast.GenDecl:
+			tokenContainingNodes = append(tokenContainingNodes, n)
+		}
+		return true
+	})
+	return
+}
+
+func chooseNewTokenAndAssign(n ast.Node) (newToken token.Token) {
+	switch n := n.(type) {
+	case *ast.BasicLit:
+		newToken = *utl.PickExcept(tokenw.AcceptedByBasicLit, []token.Token{n.Kind})
+		n.Kind = newToken
+	case *ast.UnaryExpr:
+		newToken = *utl.PickExcept(tokenw.AcceptedByUnaryExpr, []token.Token{n.Op})
+		n.Op = newToken
+	case *ast.BinaryExpr:
+		newToken = *utl.PickExcept(tokenw.AcceptedByBinaryExpr, []token.Token{n.Op})
+		n.Op = newToken
+	case *ast.IncDecStmt:
+		newToken = *utl.PickExcept(tokenw.AcceptedByIncDecStmt, []token.Token{n.Tok})
+		n.Tok = newToken
+	case *ast.AssignStmt:
+		newToken = *utl.PickExcept(tokenw.AcceptedByAssignStmt, []token.Token{n.Tok})
+		n.Tok = newToken
+	case *ast.BranchStmt:
+		newToken = *utl.PickExcept(tokenw.AcceptedByBranchStmt, []token.Token{n.Tok})
+		n.Tok = newToken
+	case *ast.RangeStmt:
+		newToken = *utl.PickExcept(tokenw.AcceptedByRangeStmt, []token.Token{n.Tok})
+		n.Tok = newToken
+	case *ast.GenDecl: // what are the chances for an import statement can work as a type declaration?
+		newToken = *utl.PickExcept(tokenw.AcceptedByGenDecl, []token.Token{n.Tok})
+		n.Tok = newToken
+	}
+	return
+}
+
+func Perform(n ast.Node) (changedNode ast.Node, newToken token.Token, ok bool) {
+	// list available
+	tokenContainingNodes := listTokenContainingNodes(n)
+	if len(tokenContainingNodes) == 0 {
+		return changedNode, newToken, false
+	}
+	changedNode = *utl.Pick(tokenContainingNodes)
+	newToken = chooseNewTokenAndAssign(changedNode)
+	return changedNode, newToken, true
+}
---
+++ internal/genetics/mutation/token_shuffle/token_shuffle_test.go
@@ -0,0 +1,76 @@
+package token_shuffle
+
+import (
+	"fmt"
+	"reflect"
+	"tde/internal/astw/clone"
+	ast_utl "tde/internal/astw/utilities"
+
+	"go/ast"
+	"testing"
+
+	"github.com/kylelemons/godebug/diff"
+	"github.com/pkg/errors"
+)
+
+func loadTestPackage() (*ast.Package, *ast.File, *ast.FuncDecl, error) {
+	_, astPkgs, err := ast_utl.LoadDir("../../../test_package")
+	if err != nil {
+		return nil, nil, nil, errors.Wrapf(err, "could not load test package")
+	}
+	astPkg := astPkgs["test_package"]
+	astFile := astPkg.Files["../test_package/walk.go"]
+	funcDecl, err := ast_utl.FindFuncDecl(astPkg, "walkHelper")
+	if err != nil {
+		return nil, nil, nil, errors.Wrap(err, "could not find test function")
+	}
+	return astPkg, astFile, funcDecl, nil
+}
+
+func Test_Operator(t *testing.T) {
+	_, _, originalFuncDecl, err := loadTestPackage()
+	if err != nil {
+		t.Error(errors.Wrapf(err, "prep"))
+	}
+
+	modifiedFuncDecl := clone.FuncDecl(originalFuncDecl)
+	choosenNode, newToken, ok := Perform(modifiedFuncDecl.Body)
+	if !ok {
+		t.Error("return value")
+	}
+
+	codeForOriginal, err := ast_utl.String(originalFuncDecl)
+	if err != nil {
+		t.Error("validation prep")
+	}
+	codeForModified, err := ast_utl.String(modifiedFuncDecl)
+	if err != nil {
+		t.Error("validation prep")
+	}
+
+	fmt.Println("Differences in code:\n", diff.Diff(codeForOriginal, codeForModified))
+
+	if ast_utl.CompareRecursively(originalFuncDecl, modifiedFuncDecl) {
+		t.Error("validation", choosenNode, newToken)
+	}
+
+}
+
+func Test_Bulk(t *testing.T) {
+	_, _, originalFuncDecl, err := loadTestPackage()
+	if err != nil {
+		t.Error(errors.Wrapf(err, "prep"))
+	}
+
+	for i := 0; i < 1000; i++ {
+		modifiedFuncDecl := clone.FuncDecl(originalFuncDecl)
+		choosenNode, newToken, ok := Perform(modifiedFuncDecl.Body)
+		if !ok {
+			t.Error("return value")
+		}
+		if ast_utl.CompareRecursively(originalFuncDecl, modifiedFuncDecl) {
+			t.Errorf("validation i='%d' typeOf->choosenNode='%v' address->choosenNode='%p' newToken='%v'", i, reflect.TypeOf(choosenNode), choosenNode, newToken)
+		}
+	}
+
+}
