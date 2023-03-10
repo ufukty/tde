d347261e24c286db9a19680dfc6885055cdbb7cb
Ufuk <ufukty@gmail.com>
2023-03-10 18:49:08 +0300

    genetics/mutation/remove_line

---
+++ internal/genetics/mutation/remove_line/remove_line.go
@@ -0,0 +1,52 @@
+package remove_line
+
+import (
+	"go/ast"
+	"tde/internal/utilities"
+)
+
+func list(n ast.Node) (stmtSliceContainingSubnodes []ast.Node) {
+	ast.Inspect(n, func(n ast.Node) bool {
+		switch n := n.(type) {
+		case *ast.BlockStmt:
+			if len(n.List) > 0 {
+				stmtSliceContainingSubnodes = append(stmtSliceContainingSubnodes, n)
+			}
+		case *ast.CommClause:
+			if len(n.Body) > 0 {
+				stmtSliceContainingSubnodes = append(stmtSliceContainingSubnodes, n)
+			}
+		case *ast.CaseClause:
+			if len(n.Body) > 0 {
+				stmtSliceContainingSubnodes = append(stmtSliceContainingSubnodes, n)
+			}
+		}
+
+		return true
+	})
+	return
+}
+
+func removeOneLine(n ast.Node) {
+	switch n := n.(type) {
+	case *ast.BlockStmt:
+		cutPoint := utilities.URandIntN(len(n.List))
+		n.List = append(n.List[:cutPoint], n.List[cutPoint+1:]...)
+	case *ast.CommClause:
+		cutPoint := utilities.URandIntN(len(n.Body))
+		n.Body = append(n.Body[:cutPoint], n.Body[cutPoint+1:]...)
+	case *ast.CaseClause:
+		cutPoint := utilities.URandIntN(len(n.Body))
+		n.Body = append(n.Body[:cutPoint], n.Body[cutPoint+1:]...)
+	}
+}
+
+func RemoveLine(n ast.Node) (ok bool) {
+	stmtSliceContainingSubnodes := list(n)
+	if len(stmtSliceContainingSubnodes) == 0 {
+		return false
+	}
+	choosenNode := *utilities.Pick(stmtSliceContainingSubnodes)
+	removeOneLine(choosenNode)
+	return true
+}
---
+++ internal/genetics/mutation/remove_line/remove_line_test.go
@@ -0,0 +1,74 @@
+package remove_line
+
+import (
+	"fmt"
+	"go/ast"
+	"tde/internal/astw/clone"
+	ast_utl "tde/internal/astw/utilities"
+	"testing"
+
+	"github.com/kylelemons/godebug/diff"
+	"github.com/pkg/errors"
+)
+
+func loadTestPackage() (*ast.Package, *ast.File, *ast.FuncDecl, error) {
+	_, astPkgs, err := ast_utl.LoadDir("../../../test_package")
+	if err != nil {
+		return nil, nil, nil, errors.Wrapf(err, "could not load test package")
+	}
+	astPkg := astPkgs["test_package"]
+	astFile := astPkg.Files["../test_package/walk.go"]
+	funcDecl, err := ast_utl.FindFuncDecl(astPkg, "walkHelper")
+	if err != nil {
+		return nil, nil, nil, errors.Wrap(err, "could not find test function")
+	}
+	return astPkg, astFile, funcDecl, nil
+}
+
+func Test_RemoveLine(t *testing.T) {
+	_, _, originalFuncDecl, err := loadTestPackage()
+	if err != nil {
+		t.Error(errors.Wrapf(err, "prep"))
+	}
+
+	modifiedFuncDecl := clone.FuncDecl(originalFuncDecl)
+	ok := RemoveLine(modifiedFuncDecl.Body)
+	if !ok {
+		t.Error("return value")
+	}
+
+	codeForOriginal, err := ast_utl.String(originalFuncDecl)
+	if err != nil {
+		t.Error("validation prep")
+	}
+	codeForModified, err := ast_utl.String(modifiedFuncDecl)
+	if err != nil {
+		t.Error("validation prep")
+	}
+
+	fmt.Println("Differences in code:\n", diff.Diff(codeForOriginal, codeForModified))
+
+	if ast_utl.CompareRecursively(originalFuncDecl, modifiedFuncDecl) {
+		t.Error("validation")
+	}
+}
+
+func Test_RemoveLineMany(t *testing.T) {
+	_, _, originalFuncDecl, err := loadTestPackage()
+	if err != nil {
+		t.Error(errors.Wrapf(err, "prep"))
+	}
+
+	for i := 0; i < 1000; i++ {
+		modifiedFuncDecl := clone.FuncDecl(originalFuncDecl)
+		
+		ok := RemoveLine(modifiedFuncDecl.Body)
+		if !ok {
+			t.Error("return value")
+		}
+		
+		if ast_utl.CompareRecursively(originalFuncDecl, modifiedFuncDecl) {
+			t.Error("validation", i)
+		}
+	}
+}
