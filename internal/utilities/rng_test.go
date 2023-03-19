cbcf98c35bb436dd61408f15a1ba2fb57c2c9abe
Ufuk <ufukty@gmail.com>
2023-03-19 16:09:52 +0300

    utilities:PickWeightedIndex test for float weights

--- internal/utilities/rng_test.go
+++ internal/utilities/rng_test.go
@@ -53,7 +53,7 @@ func Test_Pick(t *testing.T) {
 	fmt.Println(freq)
 }
 
-func Test_PickExcept(t *testing.T) {
+func Test_PickExceptInt(t *testing.T) {
 	examples := []struct {
 		Slice  []int
 		Except []int
@@ -90,6 +90,49 @@ func TestWeightedIndex(t *testing.T) {
 	}
 
 	for i, weights := range testCases {
+		counters := make([]int64, len(weights))
+
+		for j := 0; j < totalRunPerCase; j++ {
+			index := PickWeightedIndex(weights)
+			counters[index]++
+		}
+
+		expectedFrequencies := ProportionItemsToTotal(weights)
+		gotFrequencies := ProportionItemsToTotal(counters)
+
+		fmt.Printf("i=%d, weights=%v, counters=%v, frequencies:\n\texpected : ", i, weights, counters)
+		for j := 0; j < len(weights); j++ {
+			fmt.Printf("%d%% ", int(expectedFrequencies[j]*100))
+		}
+		fmt.Printf("\n\tgot      : ")
+		for j := 0; j < len(weights); j++ {
+			fmt.Printf("%d%% ", int(gotFrequencies[j]*100))
+		}
+		fmt.Println()
+
+		for j := 0; j < len(weights); j++ {
+			expectedFrequency, gotFrequency := expectedFrequencies[j], gotFrequencies[j]
+			if !(0.8*expectedFrequency <= gotFrequency && gotFrequency <= expectedFrequency*1.2) {
+				t.Errorf("validation. frequency of %d. index", j)
+			}
+		}
+	}
+}
+
+func TestWeightedIndexFloat64(t *testing.T) {
+	const totalRunPerCase = 10000
+
+	testCasesFloat64 := [][]float64{
+		{0.4, 0.5, 0.2, 0.7, 0.8, 0.9, 0.6, 0.3, 0.1, 0.2},
+		{0.0, 0.1},
+		{0.1},
+		{0.1, 0.1, 0.1, 0.1, 0.1},
+		{0.9, 0.1},
+		{0.3, 0.2, 0.1, 0.0},
+		{0.0, 0.1, 0.0},
+	}
+
+	for i, weights := range testCasesFloat64 {
 		counters := make([]int64, len(weights))
 
 		for j := 0; j < totalRunPerCase; j++ {
