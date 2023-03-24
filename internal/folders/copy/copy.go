8b0248e61340b7483ca71e0864c996d35159f298
Ufuk <ufukty@gmail.com>
2023-03-24 18:00:58 +0300

    folders/copy

---
+++ internal/folders/copy/copy.go
@@ -0,0 +1,52 @@
+package copy
+
+import (
+	ucopy "tde/internal/utilities/copy"
+
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+	"golang.org/x/exp/slices"
+)
+
+var DefaultSkipDirs = []string{".git", "build", "docs", ".vscode"}
+
+func Module(dstMod string, srcMod string, includeSubfolders bool, skipDirs []string) error {
+	return filepath.Walk(srcMod, func(srcAbs string, fileInfo os.FileInfo, err error) error {
+		if err != nil {
+			return errors.Wrap(err, "failed to walk directory")
+		}
+
+		srcRel, err := filepath.Rel(srcMod, srcAbs)
+		if err != nil {
+			return errors.Wrap(err, "getting relative path from absolute")
+		}
+
+		dstAbs := filepath.Join(dstMod, srcRel)
+
+		switch fileInfo.Mode() & os.ModeType {
+		case os.ModeDir:
+			if !includeSubfolders || slices.Index(skipDirs, srcRel) != -1 {
+				log.Println("skip dir:", srcRel)
+				return filepath.SkipDir
+			}
+			log.Println("copy dir:", srcRel)
+			if err := ucopy.CreateIfNotExists(dstAbs, 0755); err != nil {
+				return errors.Wrap(err, "CreateIfNotExists")
+			}
+
+		case os.ModeSymlink:
+			break
+
+		default:
+			log.Println("copy file:", srcRel)
+			if err := ucopy.File(srcAbs, dstAbs); err != nil {
+				return errors.Wrap(err, "File")
+			}
+		}
+
+		return nil // keep walk
+	})
+}
---
+++ internal/folders/copy/copy_test.go
@@ -0,0 +1,20 @@
+package copy
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/pkg/errors"
+)
+
+func TestModule(t *testing.T) {
+	dst := filepath.Join(os.TempDir(), "tde_test_folders_copy_copy_module")
+	fmt.Println("destination:", dst)
+
+	err := Module(dst, "../../../", true, DefaultSkipDirs)
+	if err != nil {
+		t.Error(errors.Wrapf(err, "return value"))
+	}
+}
