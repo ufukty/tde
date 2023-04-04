23178e810235c75ba75a8989e73becdc9f493321
Ufuk <ufukty@gmail.com>
2023-04-04 11:33:23 +0300

    cmd/poc/produce removed

--- cmd/poc/internal/prepare/command.go
+++ cmd/poc/internal/prepare/command.go
@@ -17,6 +17,16 @@ type Command struct {
 	N        int                 `short:"n" default:"10"`
 	Exclude  command.MultiString `long:"exclude" short:"e"` // TODO:
 	TestName string              `precedence:"0"`
+
+	Timeout    int                 `long:"timeout" default:"10"`
+	Runner     string              `long:"runner"`
+	Continue   string              `long:"continue" short:"c" default:"10"`
+	Model      string              `long:"model" default:"0.1"`
+	Ratios     string              `long:"ratios" default:"10/1"`
+	Population int                 `long:"population" default:"1000"`
+	Iterate    int                 `long:"iterate" default:"10"`
+	Size       int                 `long:"size" default:"1000"`
+	Package    command.MultiString `long:"package" short:"p"` // packages allowed to import
 }
 
 func (c *Command) Run() {
