fcc9d88bd41b1db029f1cf82b144a86a48f001e3
Ufuk <ufukty@gmail.com>
2023-04-04 22:55:23 +0300

    astw/utilities/find find from package node with file information

--- internal/astw/utilities/find.go
+++ internal/astw/utilities/find.go
@@ -28,3 +28,34 @@ func FindFuncDecl(toppest ast.Node, name string) (*ast.FuncDecl, error) {
 		return found[0], nil
 	}
 }
+
+func FindFuncDeclInPkg(pkg *ast.Package, name string) (*ast.File, *ast.FuncDecl, error) {
+	funcDecl := []*ast.FuncDecl{}
+	file := []*ast.File{}
+
+	var fileVisited *ast.File
+
+	ast.Inspect(pkg, func(n ast.Node) bool {
+		if n != nil {
+			if file, ok := n.(*ast.File); ok {
+				fileVisited = file
+			}
+			if functionDeclaration, ok := n.(*ast.FuncDecl); ok {
+				if functionDeclaration.Name.Name == name {
+					funcDecl = append(funcDecl, functionDeclaration)
+					file = append(file, fileVisited)
+					return false
+				}
+			}
+		}
+		return true
+	})
+
+	if len(funcDecl) == 0 {
+		return nil, nil, fmt.Errorf("could not find function '%s'", name)
+	} else if len(funcDecl) > 1 {
+		return nil, nil, fmt.Errorf("more than one function definition with the name  '%s'", name)
+	} else {
+		return file[0], funcDecl[0], nil
+	}
+}
