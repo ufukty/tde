679f787676a793c08201e8fd84421ef57e1b79f7
Ufuk <ufukty@gmail.com>
2023-04-13 23:12:04 +0300

    platform/provisioning

--- platform/provisioning/terraform.tf
+++ platform/provisioning/terraform.tf
@@ -7,29 +7,84 @@ terraform {
   }
 }
 
-variable "DSDS" {}
+# ------------------------------------------------------------- #
+# Variables
+# ------------------------------------------------------------- #
 
-variable "DIGITALOCEAN_THESIS_TOKEN" {} // env var
+variable "DIGITALOCEAN_THESIS_TOKEN" { // env var
+}
+
+# ------------------------------------------------------------- #
+# Locals
+# ------------------------------------------------------------- #
+
+locals {
+  region           = "fra1"
+  slug             = "s-1vcpu-1gb"
+  user_name        = "a4v95e281o7hvmc"
+  base_image_name  = "thesis-base-focal-64"
+  date_time_string = formatdate("YY-MM-DD-hh-mm-ss", timestamp())
+  ssh_fingerprints = ["42:75:b8:ad:c1:76:4b:58:07:ec:e9:85:66:27:9b:e6"]
+}
+
+# ------------------------------------------------------------- #
+# Main
+# ------------------------------------------------------------- #
 
 provider "digitalocean" {
   # Configuration options
   token = var.DIGITALOCEAN_THESIS_TOKEN
 }
 
+data "digitalocean_droplet_snapshot" "last_snapshot" {
+  name_regex  = "^packer-${local.base_image_name}-[0-9]*"
+  region      = local.region
+  most_recent = true
+}
+
+resource "digitalocean_vpc" "vpc" {
+  name   = "thesis-vpc"
+  region = local.region
+}
+
 resource "digitalocean_droplet" "runners" {
   count = 1
 
-  image  = "ubuntu-uptodate-focal-64-1681391363"
+  image  = data.digitalocean_droplet_snapshot.last_snapshot.id
   name   = "thesis-runner-${count.index}"
-  region = "fra1" // ams3
+  region = local.region
   size   = "s-1vcpu-1gb"
-  tags   = ["thesis"]
+  tags   = ["thesis", "thesis-runner"]
+
+  ipv6        = true
+  backups     = false
+  monitoring  = true
+  resize_disk = false
+  ssh_keys    = local.ssh_fingerprints
+  vpc_uuid    = digitalocean_vpc.vpc.id
 }
 
 resource "digitalocean_droplet" "evolution" {
-  image  = "ubuntu-uptodate-focal-64-1681391363"
+  image  = data.digitalocean_droplet_snapshot.last_snapshot.id
   name   = "thesis-evolution"
-  region = "fra1" // ams3
+  region = local.region
   size   = "s-1vcpu-1gb"
-  tags   = ["thesis"]
+  tags   = ["thesis", "thesis-evolution"]
+
+  ipv6        = true
+  backups     = false
+  monitoring  = true
+  resize_disk = false
+  ssh_keys    = local.ssh_fingerprints
+  vpc_uuid    = digitalocean_vpc.vpc.id
+}
+
+resource "local_file" "inventory" {
+  content = templatefile("${path.module}/inventory.template.cfg",
+    {
+      runners   = digitalocean_droplet.runners.*.ipv4_address
+      evolution = digitalocean_droplet.evolution.*.ipv4_address
+    }
+  )
+  filename = "inventory.cfg"
 }
