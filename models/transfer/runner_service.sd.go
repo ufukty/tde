32f28cc5ca55f37a518bc98866ebb516a144fadc
Ufuk <ufukty@gmail.com>
2023-04-23 23:39:41 +0300

    models/transfer

--- models/transfer/runner_service.sd.go
+++ models/transfer/runner_service.sd.go
@@ -4,31 +4,46 @@ import (
 	"bytes"
 	"encoding/json"
 	"net/http"
-
 	"github.com/pkg/errors"
 )
 
-func (s *RunnerService_NewTest_Request) NewRequest(method, url string) (*http.Request, error) {
+func (req *RunnerService_NewTest_Request) NewRequest(method, url string) (*http.Request, error) {
 	buffer := new(bytes.Buffer)
-	err := json.NewEncoder(buffer).Encode(s)
+	err := json.NewEncoder(buffer).Encode(req)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed on serialization")
 	}
-	req, err := http.NewRequest(method, url, buffer)
+	httpRequest, err := http.NewRequest(method, url, buffer)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed on creating request object")
 	}
-	return req, nil
+	return httpRequest, nil
 }
-func (s *RunnerService_NewTest_Request) ParseRequest(r *http.Request) error {
-	err := json.NewDecoder(r.Body).Decode(s)
+func (req *RunnerService_NewTest_Request) ParseRequest(r *http.Request) error {
+	err := json.NewDecoder(r.Body).Decode(req)
 	if err != nil {
-		return errors.Wrap(err, "failed on serialization")
+		return errors.Wrap(err, "failed on parsing the response body")
 	}
 	return nil
 }
-func (s *RunnerService_NewTest_Response) SerializeIntoResponseWriter(w http.ResponseWriter) error {
-	err := json.NewEncoder(w).Encode(s)
+func (req *RunnerService_NewTest_Request) Send(method, url string) (*RunnerService_NewTest_Response, error) {
+	httpRequest, err := req.NewRequest(method, url)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed on creating an object for request")
+	}
+	httpResponse, err := http.DefaultClient.Do(httpRequest)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed on sending the request")
+	}
+	res := RunnerService_NewTest_Response{}
+	err = res.DeserializeResponse(httpResponse)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed on parsing the response body")
+	}
+	return &res, nil
+}
+func (res *RunnerService_NewTest_Response) SerializeIntoResponseWriter(w http.ResponseWriter) error {
+	err := json.NewEncoder(w).Encode(res)
 	if err != nil {
 		return errors.Wrap(err, "failed on serialization")
 	}
@@ -37,7 +52,7 @@ func (s *RunnerService_NewTest_Response) SerializeIntoResponseWriter(w http.Resp
 func (s *RunnerService_NewTest_Response) DeserializeResponse(res *http.Response) error {
 	err := json.NewDecoder(res.Body).Decode(s)
 	if err != nil {
-		return errors.Wrap(err, "failed on serialization")
+		return errors.Wrap(err, "failed on parsing the response body")
 	}
 	return nil
 }
