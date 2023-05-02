32f48da1ff8c2eb5c2181dfb18c7876dcbaf8200
Ufuk <ufukty@gmail.com>
2023-05-03 00:02:13 +0300

    cmd/pow init (proof-of-work endpoint)

---
+++ cmd/pow/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"tde/internal/router"
+
+	"github.com/gorilla/mux"
+)
+
+
+
+func main() {
+	router.StartRouter(":8085", func(r *mux.Router) {
+		r.PathPrefix("/").Methods("GET").HandlerFunc()
+		r.PathPrefix("/").Methods("POST").HandlerFunc()
+	})
+}
