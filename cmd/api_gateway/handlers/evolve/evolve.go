135eb78911eb15691274151aa0df1c7025d53b66
Ufuk <13807261+ufukty@users.noreply.github.com>
2023-05-08 17:23:20 +0300

    cmd/api_gateway

---
+++ cmd/api_gateway/handlers/evolve/evolve.go
@@ -0,0 +1,7 @@
+package evolve
+
+import "net/http"
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ cmd/api_gateway/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	handler_evolve "tde/cmd/api_gateway/handlers/evolve"
+	"tde/internal/router"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router.StartRouter(":80", func(r *mux.Router) {
+		sub := r.PathPrefix("/api").Subrouter()
+
+		sub.PathPrefix("/evolve").HandlerFunc(handler_evolve.Handler)
+	})
+}
