b909e3640a2063f0edf0990f1306114df71ee145
Ufuk <13807261+ufukty@users.noreply.github.com>
2023-05-11 14:33:04 +0300

    int/ms/service_discovery: fix verbose logging

--- internal/microservices/service-discovery/service_discovery.go
+++ internal/microservices/service-discovery/service_discovery.go
@@ -33,7 +33,6 @@ func (sd *ServiceDiscovery) readConfig() {
 	if !sd.updateLock.TryLock() {
 		return
 	}
-	log.Println("ServiceDiscovery: reading config file")
 	fileHandler, err := os.Open(sd.configPath)
 	if err != nil {
 		log.Fatalln(errors.Wrapf(err, "Failed to open '%s'", sd.configPath))
@@ -43,7 +42,6 @@ func (sd *ServiceDiscovery) readConfig() {
 	if err != nil {
 		log.Fatalln(errors.Wrapf(err, "Failed to decode %s", sd.configPath))
 	}
-	log.Println("ServiceDiscovery: reading config file (done)")
 	sd.updateLock.Unlock()
 }
 
