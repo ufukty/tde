3ab8c62b5dc1a2343ea9cfd768b1da51d860abca
Ufuk <13807261+ufukty@users.noreply.github.com>
2023-05-22 00:37:29 +0300

    platform/stage/provision/vpn

--- platform/stage/provisioning/vpn/post-provision.sh
+++ platform/stage/provisioning/vpn/post-provision.sh
@@ -1,7 +1,4 @@
 IPV4_ADDRESS_PRIVATE="${IPV4_ADDRESS_PRIVATE:?"Required"}"
 
-set -e # stop script execution when a command fails
-
 ssh-keygen -R "$IPV4_ADDRESS_PRIVATE"
 ssh-keyscan "$IPV4_ADDRESS_PRIVATE" >>~/.ssh/known_hosts
-
