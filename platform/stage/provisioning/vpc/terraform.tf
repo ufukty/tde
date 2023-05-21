1f8006c5c65677420487b45ece8ac27140bbec3b
Ufuk <13807261+ufukty@users.noreply.github.com>
2023-05-21 17:31:40 +0300

    platform/stage/provision/vpc

--- platform/stage/provisioning/vpc/terraform.tf
+++ platform/stage/provisioning/vpc/terraform.tf
@@ -7,32 +7,49 @@ terraform {
   }
 }
 
-
 variable "project_prefix" {
   type = string
 }
 
-variable "vpc_details" {
+variable "digitalocean" {
   type = object({
-    do = object({
-      sfo2 = object({ range = string })
-      sfo3 = object({ range = string })
-      tor1 = object({ range = string })
-      nyc1 = object({ range = string })
-      nyc3 = object({ range = string })
-      lon1 = object({ range = string })
-      ams3 = object({ range = string })
-      fra1 = object({ range = string })
-      blr1 = object({ range = string })
-      sgp1 = object({ range = string })
+    activated_regions = object({
+      vpc = set(string)
+      vpn = set(string)
+    })
+    config = object({
+      vpc = object({
+        sfo2 = object({ range = string })
+        sfo3 = object({ range = string })
+        tor1 = object({ range = string })
+        nyc1 = object({ range = string })
+        nyc3 = object({ range = string })
+        lon1 = object({ range = string })
+        ams3 = object({ range = string })
+        fra1 = object({ range = string })
+        blr1 = object({ range = string })
+        sgp1 = object({ range = string })
+      })
+      vpn = object({
+        sfo2 = object({ subnet_address = string })
+        sfo3 = object({ subnet_address = string })
+        tor1 = object({ subnet_address = string })
+        nyc1 = object({ subnet_address = string })
+        nyc3 = object({ subnet_address = string })
+        lon1 = object({ subnet_address = string })
+        ams3 = object({ subnet_address = string })
+        fra1 = object({ subnet_address = string })
+        blr1 = object({ subnet_address = string })
+        sgp1 = object({ subnet_address = string })
+      })
     })
   })
 }
 
 resource "digitalocean_vpc" "vpc" {
-  for_each = toset(keys(var.vpc_details.do))
+  for_each = var.digitalocean.activated_regions.vpc
 
   name     = "${var.project_prefix}-${each.value}"
   region   = each.value
-  ip_range = var.vpc_details.do[each.value].range
+  ip_range = var.digitalocean.config.vpc[each.value].range
 }
