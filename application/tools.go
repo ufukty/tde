f641dddca259e69e389364db48c7ee03b580cc0a
Ufuk <13807261+ufukty@users.noreply.github.com>
2023-06-13 18:04:09 +0300

    app/buf & app/internal/api protobuf artifacts removed

--- application/tools.go
+++ application/tools.go
@@ -4,10 +4,5 @@
 package tools
 
 import (
-	_ "github.com/envoyproxy/protoc-gen-validate"
-	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
-	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
 	_ "golang.org/x/tools/cmd/stringer"
-	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
-	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
