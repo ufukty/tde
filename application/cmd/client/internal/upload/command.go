0411e226b05fe5faaac76e79b036a74577ab8980
Ufuk <13807261+ufukty@users.noreply.github.com>
2023-06-28 00:13:43 +0300

    app/cmd/client/internal/upload fix gateway address

--- application/cmd/client/internal/upload/command.go
+++ application/cmd/client/internal/upload/command.go
@@ -55,7 +55,7 @@ func (c *Command) Run() {
 	}
 
 	log.Println("Uploading...")
-	resp, err = req.Send("POST", "http://127.0.0.1:8080/api/v1.0.0/customs/module")
+	resp, err = req.Send("POST", "http://127.0.0.1:8087/api/v1.0.0/customs/module")
 	if err != nil {
 		log.Fatalln(errors.Wrap(err, "Failed"))
 	}
