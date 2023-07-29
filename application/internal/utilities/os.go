44904bb55e05c39de92d44caecf4a0c1e89405bd
Ufuk <13807261+ufukty@users.noreply.github.com>
2023-07-29 17:49:05 +0300

    app/int/utilities new: Files, Dirs, IndentLines

--- application/internal/utilities/os.go
+++ application/internal/utilities/os.go
@@ -1,6 +1,10 @@
 package utilities
 
 import (
+	"fmt"
+	"io/fs"
+	"os"
+
 	"github.com/pkg/errors"
 )
 
@@ -10,4 +14,20 @@ func WorkingDir() (string, error) {
 		return "", errors.Wrap(err, "Failed to run command pwd")
 	}
 	return stdOut, nil
+}
+
+func Dirs(path string) ([]fs.DirEntry, error) {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return nil, fmt.Errorf("listing dir entries: %w", err)
+	}
+	return FilteredMap(entries, func(i int, entry fs.DirEntry) (fs.DirEntry, bool) { return entry, entry.IsDir() }), nil
+}
+
+func Files(path string) ([]fs.DirEntry, error) {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return nil, fmt.Errorf("listing dir entries: %w", err)
+	}
+	return FilteredMap(entries, func(i int, entry fs.DirEntry) (fs.DirEntry, bool) { return entry, !entry.IsDir() }), nil
 }
--- application/internal/utilities/strings.go
+++ application/internal/utilities/strings.go
@@ -1,8 +1,20 @@
 package utilities
 
+import "strings"
+
 func StringFold(str string, partLength int) (splitted []string) {
 	for i := 0; i < len(str); i += partLength {
 		splitted = append(splitted, str[i:Min(i+partLength, len(str))])
 	}
 	return
+}
+
+func IndentLines(str string, indent int) string {
+	indentation := ""
+	for i := 0; i < indent; i++ {
+		indentation += " "
+	}
+	return strings.Join(Map(strings.Split(str, "\n"), func(i int, line string) string {
+		return indentation + line
+	}), "\n")
 }
