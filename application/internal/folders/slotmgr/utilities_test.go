4dc9c17b7b52fbcd735a4c0acc147e71862bee37
Ufuk <13807261+ufukty@users.noreply.github.com>
2023-08-01 08:06:33 +0300

    app/int/folders/slotmgr test case

--- application/internal/folders/slotmgr/utilities_test.go
+++ application/internal/folders/slotmgr/utilities_test.go
@@ -14,33 +14,36 @@ func Test_ReplaceSectionInFile(t *testing.T) {
 	testcases := []string{
 		"testdata/case1",
 		"testdata/case2", // ends with a linefeed
+		"testdata/case3", // ends with 3 empty lines
 	}
 	for _, testcase := range testcases {
-		inputfile, err := CreateSwapFile(filepath.Join(testcase, "input.txt"))
-		if err != nil {
-			t.Fatal(fmt.Errorf("%s prep: %w", testcase, err))
-		}
-		fmt.Println("swap file for input:", inputfile)
-		want, err := os.ReadFile(filepath.Join(testcase, "want.txt"))
-		if err != nil {
-			t.Fatal(fmt.Errorf("%s prep: %w", testcase, err))
-		}
-		modifications, err := os.ReadFile(filepath.Join(testcase, "contents.txt"))
-		if err != nil {
-			t.Fatal(fmt.Errorf("%s prep: %w", testcase, err))
-		}
-		err = ReplaceSectionInFile(inputfile, 10, 20, modifications) // lines [10, 20)
-		if err != nil {
-			t.Fatal(fmt.Errorf("%s act: %w", testcase, err))
-		}
-		got, err := os.ReadFile(inputfile)
-		if err != nil {
-			t.Fatal(fmt.Errorf("%s assert prep: %w", testcase, err))
-		}
-		if slices.Compare(want, got) != 0 {
-			fmt.Println(testcase, " diff want->got '+', '-', or ' '")
-			fmt.Println(diff.Diff(string(want), string(got)))
-			t.Fatal(fmt.Errorf("%s assert", testcase))
-		}
+		t.Run(testcase, func(t *testing.T) {
+			inputfile, err := CreateSwapFile(filepath.Join(testcase, "input.txt"))
+			if err != nil {
+				t.Fatal(fmt.Errorf("%s prep: %w", testcase, err))
+			}
+			fmt.Printf("swap file for %s: %s\n", testcase, inputfile)
+			want, err := os.ReadFile(filepath.Join(testcase, "want.txt"))
+			if err != nil {
+				t.Fatal(fmt.Errorf("%s prep: %w", testcase, err))
+			}
+			modifications, err := os.ReadFile(filepath.Join(testcase, "contents.txt"))
+			if err != nil {
+				t.Fatal(fmt.Errorf("%s prep: %w", testcase, err))
+			}
+			err = ReplaceSectionInFile(inputfile, 10, 20, modifications) // lines [10, 20)
+			if err != nil {
+				t.Fatal(fmt.Errorf("%s act: %w", testcase, err))
+			}
+			got, err := os.ReadFile(inputfile)
+			if err != nil {
+				t.Fatal(fmt.Errorf("%s assert prep: %w", testcase, err))
+			}
+			if slices.Compare(want, got) != 0 {
+				fmt.Println(testcase, " diff want->got '+', '-', or ' '")
+				fmt.Println(diff.Diff(string(want), string(got)))
+				t.Error(fmt.Errorf("%s assert", testcase))
+			}
+		})
 	}
 }
