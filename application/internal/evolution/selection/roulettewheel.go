16c23f2a6b7ce5e0ee0b8fb543da855fc93deda6
Ufuk <13807261+ufukty@users.noreply.github.com>
2023-09-25 23:24:59 +0300

    app/internal/evolution/selection roulettewheel fix probability distributions

--- application/internal/evolution/selection/roulettewheel.go
+++ application/internal/evolution/selection/roulettewheel.go
@@ -4,6 +4,7 @@ import (
 	"tde/internal/utilities"
 	models "tde/models/program"
 
+	"golang.org/x/exp/maps"
 	"golang.org/x/exp/slices"
 )
 
@@ -18,40 +19,52 @@ func getFitnesses(candidates []*models.Candidate, layer models.Layer) []float64 
 // reduces all items of the array into [0, 1] range.
 // or [1, 0] if reverse is true
 func normalizeFitnesses(fitnesses []float64, reverse bool) []float64 {
+	if len(fitnesses) == 0 {
+		return fitnesses
+	}
 	var (
-		reversedFitnesses = []float64{}
-		maxFitness        = slices.Max(fitnesses)
-		minFitness        = slices.Min(fitnesses)
-		deltaF            = maxFitness - minFitness
+		normalized = []float64{}
+		maxFitness = slices.Max(fitnesses)
+		minFitness = slices.Min(fitnesses)
+		deltaF     = maxFitness - minFitness
 	)
+	if deltaF == 0 {
+		return fitnesses
+	}
 	for _, f := range fitnesses {
 		lerped := (f - minFitness) / deltaF
 		if reverse {
 			lerped = 1.0 - lerped
 		}
-		reversedFitnesses = append(reversedFitnesses, lerped)
+		normalized = append(normalized, lerped)
 	}
-	return reversedFitnesses
+	return normalized
 }
 
 // Chooses random candidates by chances that are relative to their fitnesses
 // Candidates with higher numerical value for their fitnesses have higher chance to get selected by default.
-// When reverse is True, behaviour gets reversed.
-func RouletteWheel(candidates map[models.CandidateID]*models.Candidate, layer models.Layer, bullets int, reverse bool) []models.CandidateID {
-	var (
-		ids, cands            = utilities.MapItems(candidates)
-		fitnesses             = normalizeFitnesses(getFitnesses(cands, layer), reverse)
-		cumulativeFitnesses   = utilities.GetCumulative(fitnesses)
-		upperBoundLastFitness = cumulativeFitnesses[len(cumulativeFitnesses)-1]
-		choosenIndividuals    = []models.CandidateID{}
-		choosen               int
-		bullet                float64
-		survivors             = len(fitnesses) - bullets
-	)
-	for i := 0; i < survivors; i++ {
-		bullet = utilities.URandFloatForCrypto() * upperBoundLastFitness
-		choosen = utilities.BisectRight(cumulativeFitnesses, bullet)
-		choosenIndividuals = append(choosenIndividuals, ids[choosen])
+// When reverseFitness is True, behaviour gets reversed.
+// One candidate can get selected multiple times.
+func RouletteWheel(candidates map[models.CandidateID]*models.Candidate, layer models.Layer, reverseFitness bool) []models.CandidateID {
+	if len(candidates) == 0 {
+		return []models.CandidateID{}
 	}
-	return choosenIndividuals
+	if len(candidates) == 1 {
+		return maps.Keys(candidates)
+	}
+	var (
+		ids, cands   = utilities.MapItems(candidates)
+		fitnesses    = normalizeFitnesses(getFitnesses(cands, layer), reverseFitness)
+		cumulative   = utilities.GetCumulative(fitnesses)
+		totalFitness = cumulative[len(cumulative)-1]
+		picks        = []models.CandidateID{}
+		choosen      int
+		bullet       float64
+	)
+	for i := 0; i < len(cands); i++ {
+		bullet = utilities.URandFloatForCrypto() * totalFitness
+		choosen = utilities.BisectRight(cumulative, bullet)
+		picks = append(picks, ids[choosen])
+	}
+	return picks
 }
--- application/internal/evolution/selection/roulettewheel_test.go
+++ application/internal/evolution/selection/roulettewheel_test.go
@@ -2,6 +2,7 @@ package selection
 
 import (
 	"fmt"
+	"sort"
 	"tde/internal/utilities"
 	models "tde/models/program"
 	"testing"
@@ -10,11 +11,91 @@ import (
 	"golang.org/x/exp/slices"
 )
 
+func filterBestAndWorstIds(candidates map[models.CandidateID]*models.Candidate) (sortedIds []models.CandidateID, bests []models.CandidateID, worsts []models.CandidateID) {
+	_, cands := utilities.MapItems(candidates)
+	sort.Slice(cands, func(i, j int) bool {
+		return cands[i].Fitness.AST < cands[j].Fitness.AST
+	})
+	for _, cand := range cands {
+		sortedIds = append(sortedIds, cand.UUID)
+	}
+	for i := 0; i < int(float64(len(candidates))*0.5); i++ {
+		bests = append(bests, cands[i].UUID)
+		worsts = append(worsts, cands[len(candidates)-1-i].UUID)
+	}
+	return
+}
+
+type freqCounter struct {
+	candidates                   map[models.CandidateID]*models.Candidate
+	idFreqs                      map[models.CandidateID]int
+	sortedIds, bestIds, worstIds []models.CandidateID
+}
+
+func newFreqCounter(candidates map[models.CandidateID]*models.Candidate) *freqCounter {
+	ids := maps.Keys(candidates)
+	freqs := map[models.CandidateID]int{}
+	for _, id := range ids {
+		freqs[id] = 0
+	}
+	sortedIds, bestIds, worstIds := filterBestAndWorstIds(candidates)
+	return &freqCounter{
+		candidates: candidates,
+		idFreqs:    freqs,
+		sortedIds:  sortedIds,
+		bestIds:    bestIds,
+		worstIds:   worstIds,
+	}
+}
+
+func (fc *freqCounter) count(candidates []models.CandidateID) (survivingBest int, survivingWorst int) {
+	for _, id := range candidates {
+		fc.idFreqs[id]++
+		if slices.Contains(fc.bestIds, id) {
+			survivingBest++
+		}
+		if slices.Contains(fc.worstIds, id) {
+			survivingWorst++
+		}
+	}
+	return
+}
+
+func (fc freqCounter) PrintHistogram() {
+	if len(fc.candidates) == 0 {
+		return
+	}
+	fmt.Println("Histogram of frequencies:")
+	maxFreq := slices.Max(maps.Values(fc.idFreqs))
+	for _, id := range fc.sortedIds {
+		freq := fc.idFreqs[id]
+		fmt.Printf("    %2s %.2f %3d%% %s\n", string(id), fc.candidates[id].Fitness.AST, int(float64(freq)/float64(maxFreq)*100), utilities.StringFill("*", int(float64(freq)/float64(maxFreq)*40)))
+	}
+}
+
+func cleaned(candidates map[models.CandidateID]*models.Candidate, picks []models.CandidateID) map[models.CandidateID]*models.Candidate {
+	ret := map[models.CandidateID]*models.Candidate{}
+	maps.Copy(ret, candidates)
+	for _, id := range picks {
+		delete(ret, id)
+	}
+	return ret
+}
+
+func prepare(candidates map[models.CandidateID]*models.Candidate, picks []models.CandidateID) map[models.CandidateID]*models.Candidate {
+	ret := map[models.CandidateID]*models.Candidate{}
+	for _, id := range picks {
+		delete(ret, id)
+	}
+	return ret
+}
+
 func Test_RouletteWheelDistribution(t *testing.T) {
 	const (
-		runs    = 1000
+		run     = 1000
 		bullets = 10
 	)
+
 	var (
 		candidates = map[models.CandidateID]*models.Candidate{
 			"1":  {UUID: "1", Fitness: models.Fitness{AST: 1.0}},
@@ -58,55 +139,76 @@ func Test_RouletteWheelDistribution(t *testing.T) {
 			"39": {UUID: "39", Fitness: models.Fitness{AST: 1.0}},
 			"40": {UUID: "40", Fitness: models.Fitness{AST: 0.31}},
 		}
-		bestIds   = []models.CandidateID{"12", "19", "32", "34", "9", "27", "37", "5", "31", "3"}
-		worstIds  = []models.CandidateID{"38", "6", "10", "23", "24", "33", "29", "7", "1", "39"}
-		sortedIds = []models.CandidateID{"39", "1", "7", "29", "33", "24", "23", "10", "6", "38", "17", "13", "11", "26", "25", "40", "36", "16", "20", "4", "30", "15", "14", "28", "35", "22", "2", "18", "8", "21", "3", "31", "5", "37", "27", "9", "34", "32", "19", "12"}
-		idFreqs   = map[models.CandidateID]int{"1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0, "10": 0, "11": 0, "12": 0, "13": 0, "14": 0, "15": 0, "16": 0, "17": 0, "18": 0, "19": 0, "20": 0, "21": 0, "22": 0, "23": 0, "24": 0, "25": 0, "26": 0, "27": 0, "28": 0, "29": 0, "30": 0, "31": 0, "32": 0, "33": 0, "34": 0, "35": 0, "36": 0, "37": 0, "38": 0, "39": 0, "40": 0}
+		freqCounter = newFreqCounter(candidates)
 	)
 
 	var imbalancedRuns = 0
-	for i := 0; i < runs; i++ {
-		pickedToEliminate := RouletteWheel(candidates, models.AST, bullets, false)
+	for j := 0; j < run; j++ {
+		pickedToEliminate := RouletteWheel(candidates, models.AST, false)
 		if len(candidates)-bullets != len(pickedToEliminate) {
 			t.Fatal(fmt.Errorf("assert: len(candidates) = %d", len(pickedToEliminate)))
 		}
-
-		cleaned := map[models.CandidateID]*models.Candidate{}
-		maps.Copy(cleaned, candidates)
-		for _, id := range pickedToEliminate {
-			delete(cleaned, id)
-		}
-
-		var (
-			survivingBest  = 0
-			survivingWorst = 0
-		)
-		for id := range cleaned {
-			idFreqs[id]++
-			if slices.Contains(bestIds, id) {
-				survivingBest++
-			}
-			if slices.Contains(worstIds, id) {
-				survivingWorst++
-			}
-		}
-		if survivingWorst > survivingBest {
-			fmt.Printf("Run %d: Imbalanced: %d, %d\n", i, survivingWorst, survivingBest)
+		if survivingBest, survivingWorst := freqCounter.count(maps.Keys(cleaned(candidates, pickedToEliminate))); survivingWorst > survivingBest {
+			fmt.Printf("Run %d: Imbalanced: %d, %d\n", j, survivingWorst, survivingBest)
 			imbalancedRuns++
-		} else {
-			// fmt.Printf("Run %d: Balanced: %d, %d\n", i, survivingWorst, survivingBest)
 		}
 	}
 
-	if imbalancedRuns > runs*0.33 {
+	if imbalancedRuns > run*0.33 {
 		t.Fatal(fmt.Errorf("assert %d of the runs suffer imbalanced elimination", imbalancedRuns))
 	}
 	fmt.Printf("\nimbalanced runs: %d\n", imbalancedRuns)
+	freqCounter.PrintHistogram()
+}
 
-	fmt.Println("\nHistogram of frequencies")
-	maxFreq := slices.Max(maps.Values(idFreqs))
-	for _, id := range sortedIds {
-		freq := idFreqs[id]
-		fmt.Printf("%2s %.2f %s\n", string(id), candidates[id].Fitness.AST, utilities.StringFill("*", int(float64(freq)/float64(maxFreq)*40)))
+func candidatesForDataset(dataset []float64) map[models.CandidateID]*models.Candidate {
+	var candidates = map[models.CandidateID]*models.Candidate{}
+	for i, f := range dataset {
+		id := models.CandidateID(fmt.Sprintf("%d", i))
+		candidates[id] = &models.Candidate{UUID: id, Fitness: models.Fitness{AST: f}}
+	}
+	return candidates
+}
+
+func Test_RouletteWheelDistributionWithDatasets(t *testing.T) {
+	const (
+		ndatasets      = 10
+		runsPerDataset = 1000
+		bulletsPerRun  = 10
+	)
+
+	var datasets = [][]float64{
+		{},
+		{0.0},
+		{1.0},
+		{1.0, 0.22, 0.20, 0.28, 0.18, 0.35, 0.93, 0.21, 0.12, 0.39, 0.33, 0.0, 0.34, 0.26, 0.28, 0.30, 0.34, 0.22, 0.0, 0.29, 0.21, 0.22, 0.39, 0.39, 0.32, 0.32, 0.15, 0.24, 0.92, 0.28, 0.19, 0.0, 0.74, 0.10, 0.22, 0.30, 0.16, 0.35, 1.0, 0.31, 1.0, 0.22, 0.20, 0.28, 0.18, 0.35, 0.93, 0.21, 0.12, 0.39, 0.33, 0.0, 0.34, 0.26, 0.28, 0.30, 0.34, 0.22, 0.0, 0.29, 0.21, 0.22, 0.39, 0.39, 0.32, 0.32, 0.15, 0.24, 0.92, 0.28, 0.19, 0.0, 0.74, 0.10, 0.22, 0.30, 0.16, 0.35, 1.0, 0.31},
+		{0.000, 0.025, 0.050, 0.075, 0.100, 0.125, 0.200, 0.225, 0.250, 0.275, 0.300, 0.325, 0.350, 0.375, 0.400, 0.425, 0.450, 0.475, 0.500, 0.525, 0.550, 0.575, 0.600, 0.625, 0.650, 0.675, 0.700, 0.725, 0.750, 0.775, 0.800, 0.825, 0.850, 0.875, 0.900, 0.925, 0.950, 0.975, 1.000},
+	}
+
+	for i, dataset := range datasets {
+		fmt.Println("\nRunning the dataset:", i)
+
+		var (
+			candidates  = candidatesForDataset(dataset)
+			freqCounter = newFreqCounter(candidates)
+		)
+
+		var imbalancedRuns = 0
+		for j := 0; j < runsPerDataset; j++ {
+			picks := RouletteWheel(candidates, models.AST, true)
+			if len(candidates) != len(picks) {
+				t.Fatal(fmt.Errorf("assert: len(picks) = %d", len(picks)))
+			}
+			if survivingBest, survivingWorst := freqCounter.count(picks); survivingWorst > survivingBest {
+				fmt.Printf("Run %d: Imbalanced: %d, %d\n", j, survivingWorst, survivingBest)
+				imbalancedRuns++
+			}
+		}
+
+		if imbalancedRuns > runsPerDataset*0.33 {
+			t.Fatal(fmt.Errorf("assert %d of the runs suffer imbalanced elimination", imbalancedRuns))
+		}
+		fmt.Printf("imbalanced runs: %d\n", imbalancedRuns)
+		freqCounter.PrintHistogram()
 	}
 }
