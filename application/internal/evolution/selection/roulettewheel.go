eac3452b1befb6b276d15dbabd2bb5a043630ba2
Ufuk <13807261+ufukty@users.noreply.github.com>
2023-09-28 08:59:27 +0300

    app/internal/evolution/selection file org

--- application/internal/evolution/selection/roulettewheel.go
+++ application/internal/evolution/selection/roulettewheel.go
@@ -7,23 +7,6 @@ import (
 	"golang.org/x/exp/maps"
 )
 
-func getFitnesses(candidates []*models.Candidate, layer models.Layer) []float64 {
-	fitnesses := []float64{}
-	for _, individual := range candidates {
-		fitnesses = append(fitnesses, individual.Fitness.InLayer(layer))
-	}
-	return fitnesses
-}
-
-// reverses fitness from [0,1] to [1,0] range
-func reverse(fitnesses []float64) []float64 {
-	normalized := []float64{}
-	for _, f := range fitnesses {
-		normalized = append(normalized, 1.0-f)
-	}
-	return normalized
-}
-
 // Successful candidates should have higher fitnesses. Otherwise use reverseFitness.
 // One candidate can get selected multiple times.
 // If there is no candidate with better than worst fitness, the input returned without a difference.
---
+++ application/internal/evolution/selection/utilities.go
@@ -0,0 +1,20 @@
+package selection
+
+import models "tde/models/program"
+
+func getFitnesses(candidates []*models.Candidate, layer models.Layer) []float64 {
+	fitnesses := []float64{}
+	for _, individual := range candidates {
+		fitnesses = append(fitnesses, individual.Fitness.InLayer(layer))
+	}
+	return fitnesses
+}
+
+// reverses fitness from [0,1] to [1,0] range
+func reverse(fitnesses []float64) []float64 {
+	normalized := []float64{}
+	for _, f := range fitnesses {
+		normalized = append(normalized, 1.0-f)
+	}
+	return normalized
+}
