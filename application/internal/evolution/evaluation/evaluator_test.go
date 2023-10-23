42a69bcc4005aaebc2767b2c1afd9fde82971e7d
Ufuk <13807261+ufukty@users.noreply.github.com>
2023-10-23 17:46:59 +0300

    app/internal/evolution/evaluation clean up tests

--- application/internal/evolution/evaluation/evaluator_test.go
+++ application/internal/evolution/evaluation/evaluator_test.go
@@ -13,40 +13,39 @@ const ( // applies to all test cases in this package
 	POPULATION = 3
 )
 
-func prepare() (*Evaluator, models.Subjects, error) {
-	ctx, err := models.LoadContext("../../..", "testdata", "TDE_WordReverse")
+func prepareEvaluator(ctx *models.Context) (*Evaluator, error) {
+	pkgs, err := list.ListPackagesInDir("testdata")
 	if err != nil {
-		return nil, nil, fmt.Errorf("finding the context for package: %w", err)
+		return nil, fmt.Errorf("listing packages in target dir: %w", err)
 	}
+	sample, err := inject.WithCreatingSample(pkgs.First().Module.Dir, pkgs.First(), "TDE_WordReverse")
+	if err != nil {
+		return nil, fmt.Errorf("creating sample: %w", err)
+	}
+	sm := slotmgr.New(sample, "internal/evolution/evaluation/testdata", "words.go")
+	evaluator := NewEvaluator(sm, ctx)
+	return evaluator, nil
+}
+
+func prepareSubjects(ctx *models.Context) models.Subjects {
 	subjects := models.Subjects{}
 	for i := 0; i < POPULATION; i++ { //
 		subjects.Add(ctx.NewSubject())
 	}
-	pkgs, err := list.ListPackagesInDir("testdata")
-	if err != nil {
-		return nil, nil, fmt.Errorf("listing packages in target dir: %w", err)
-	}
-	sample, err := inject.WithCreatingSample("../../..", pkgs.First(), "TDE_WordReverse")
-	if err != nil {
-		return nil, nil, fmt.Errorf("creating sample: %w", err)
-	}
-	sm := slotmgr.New(sample, "internal/evolution/evaluation/testdata", "words.go")
-	if err := sm.PlaceSubjectsIntoSlots(subjects); err != nil {
-		return nil, nil, fmt.Errorf("passing subjects into slot manager: %w", err)
-	}
-	syntaxCheckAndProduceCode(ctx, subjects)
-	evaluator := NewEvaluator(sm, ctx)
-	return evaluator, subjects, nil
+	return subjects
 }
 
-func Test_PrintAndCompile(t *testing.T) {
-	evaluator, subjects, err := prepare()
+func prepare() (*Evaluator, models.Subjects, error) {
+	ctx, err := models.LoadContext("../../..", "testdata", "WordReverse")
 	if err != nil {
-		t.Fatal(fmt.Errorf("prep: %w", err))
+		return nil, nil, fmt.Errorf("finding the context for package: %w", err)
 	}
-	if err := evaluator.runSubjects(subjects); err != nil {
-		t.Fatal(fmt.Errorf("act: %w", err))
+	e, err := prepareEvaluator(ctx)
+	if err != nil {
+		return nil, nil, fmt.Errorf("creating evaluator: %w", err)
 	}
+	s := prepareSubjects(ctx)
+	return e, s, nil
 }
 
 // FIXME: check fitness has populated after syntax errors
@@ -55,8 +54,6 @@ func Test_Pipeline(t *testing.T) {
 	if err != nil {
 		t.Fatal(fmt.Errorf("prep: %w", err))
 	}
-
-	evaluator.sm.Print()
 
 	if err := evaluator.Pipeline(subjects); err != nil {
 		t.Fatal(fmt.Errorf("act: %w", err))
