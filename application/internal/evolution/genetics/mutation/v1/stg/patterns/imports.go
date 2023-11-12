a6dcf1d5e0c98e753ec26b03a432fd79ba824ed5
Ufuk <13807261+ufukty@users.noreply.github.com>
2023-11-12 19:09:37 +0300

    tde/internal/evolution/genetics/mutation/v1 cleans stg/patterns

--- application/internal/evolution/genetics/mutation/v1/stg/patterns/imports.go
+++
@@ -1,9 +0,0 @@
-package patterns
-
-// - TODO: Import a package
-// - TODO: Access to components that are defined in imported packages:
-//   - TODO: functions
-//   - TODO: types
-//     - TODO: aliases
-//     - TODO: structs
-//     - TODO: interfaces
