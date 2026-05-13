b518ab7250799c265f5642363c6b8bb6a4d4656a
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-05-13 10:51:09 +0300

    int/astw/astwutl: removes unused.

--- internal/astw/astwutl/subnodes.go
+++ internal/astw/astwutl/subnodes.go
@@ -6,27 +6,6 @@ import (
 	"golang.org/x/tools/go/ast/astutil"
 )
 
-func ListSubtree(root ast.Node) (subnodes []ast.Node) {
-	ast.Inspect(root, func(n ast.Node) bool {
-		if n != nil {
-			subnodes = append(subnodes, n)
-		}
-		return true
-	})
-	return
-}
-
-func ListSubtreeWithFilter(root ast.Node, filter func(n ast.Node) bool) (filteredNodes []ast.Node) {
-	list := []ast.Node{}
-	ast.Inspect(root, func(n ast.Node) bool {
-		if filter(n) {
-			list = append(list, n)
-		}
-		return true
-	})
-	return list
-}
-
 func ChildNodes(root ast.Node) (children []ast.Node) {
 	list := []ast.Node{}
 	astutil.Apply(root, func(c *astutil.Cursor) bool {
