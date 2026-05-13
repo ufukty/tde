88b9e452820239782bef61f1377e4821552f6020
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-05-13 12:23:30 +0300

    int/astw/astwutl: changes `ChildNodes` to drop dependency on external package.

--- internal/astw/astwutl/subnodes.go
+++ internal/astw/astwutl/subnodes.go
@@ -2,24 +2,16 @@ package astwutl
 
 import (
 	"go/ast"
-
-	"golang.org/x/tools/go/ast/astutil"
 )
 
-func ChildNodes(root ast.Node) (children []ast.Node) {
+func ChildNodes(root ast.Node) []ast.Node {
 	list := []ast.Node{}
-	astutil.Apply(root, func(c *astutil.Cursor) bool {
-		if c.Node() == nil {
-			return false
-		}
-		if c.Parent() == root {
-			list = append(list, c.Node())
-		}
-		if c.Node() == root {
+	ast.Inspect(root, func(n ast.Node) bool {
+		if n == root {
 			return true
-		} else {
-			return false
 		}
-	}, nil)
+		list = append(list, n)
+		return false
+	})
 	return list
 }
