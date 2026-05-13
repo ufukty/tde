3de25b25f2f80d4a152b7aa5fd312f33a3b6e288
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-05-13 15:16:30 +0300

    int/astw/astwutl: inlines `FindFunctionInFile` and removes.

--- internal/evolution/evaluation/discovery/targetfunction.go
+++ internal/evolution/evaluation/discovery/targetfunction.go
@@ -1,13 +1,36 @@
 package discovery
 
 import (
-	"tde/internal/astw/astwutl"
-
 	"fmt"
+	"go/ast"
+	"go/token"
+
+	"tde/internal/astw/astwutl"
+	"tde/internal/astw/parse"
 )
 
+func find(path string, name string) (*ast.FuncDecl, *token.FileSet, error) {
+	fset, astFile, err := parse.File(path)
+	if err != nil {
+		return nil, nil, fmt.Errorf("parsing file %q: %w", path, err)
+	}
+	var found *ast.FuncDecl
+	ast.Inspect(astFile, func(node ast.Node) bool {
+		if node, ok := node.(*ast.FuncDecl); ok {
+			if node.Name.Name == name {
+				found = node
+			}
+		}
+		return found == nil
+	})
+	if found == nil {
+		return nil, nil, fmt.Errorf("not found")
+	}
+	return found, fset, err
+}
+
 func TargetFunctionInFile(path string, funcname string) (*TargetFunction, error) {
-	funcDecl, fset, err := astwutl.FindFunctionInFile(path, funcname)
+	funcDecl, fset, err := find(path, funcname)
 	if err != nil {
 		return nil, fmt.Errorf("searching ast of file %q for the function %q: %w", path, funcname, err)
 	}
