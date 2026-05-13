f84ea7651450a23e971da2380766a22f0e42577f
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-05-13 13:03:51 +0300

    int/utils/mapw: removes unused `Diff` after inlining it.

--- internal/evolution/models/subjects.go
+++ internal/evolution/models/subjects.go
@@ -3,8 +3,6 @@ package models
 import (
 	"maps"
 	"slices"
-
-	"tde/internal/utilities/mapw"
 )
 
 type Subjects map[Sid]*Subject // To make Subjects accessible by CIDs
@@ -18,8 +16,13 @@ func (s Subjects) Join(s2 Subjects) {
 }
 
 func (s Subjects) Diff(subtract Subjects) Subjects {
-	diff := mapw.Diff(map[Sid]*Subject(s), map[Sid]*Subject(subtract))
-	return (Subjects)(diff)
+	d := make(Subjects, len(s))
+	for k := range s {
+		if _, ok := subtract[k]; !ok {
+			d[k] = s[k]
+		}
+	}
+	return d
 }
 
 func SubjectsFrom(s []*Subject) Subjects {
--- internal/utilities/mapw/mapw.go
+++ internal/utilities/mapw/mapw.go
@@ -1,15 +1,5 @@
 package mapw
 
-// returns m / c, with O(n)
-func Diff[M map[K]V, K comparable, V any](m, c M) M {
-	d := make(M, len(m))
-	for k := range m {
-		if _, ok := c[k]; !ok {
-			d[k] = m[k]
-		}
-	}
-	return d
-}
 
 func FindKey[K, V comparable](m map[K]V, v V) (K, bool) {
 	for k, v1 := range m {
