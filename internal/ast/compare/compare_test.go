96ee876e12f93763a6622bc15925774c29af686d
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-05-13 18:17:08 +0300

    int/ast/compare: modernizes the test structure.

--- internal/ast/compare/compare_test.go
+++ internal/ast/compare/compare_test.go
@@ -1,6 +1,7 @@
 package compare
 
 import (
+	"fmt"
 	"go/ast"
 	"go/token"
 	"testing"
@@ -8,144 +9,104 @@ import (
 	"tde/internal/ast/clone"
 )
 
-const TEST_FILE = `package main
-	
-func Addition(a, b int) int {
-	return a + b
-}
-`
-
 var (
-	TEST_TREE_OBJ_FUNC = ast.NewObj(ast.Fun, "Addition")
-	TEST_TREE_OBJ_A    = ast.NewObj(ast.Var, "a")
-	TEST_TREE_OBJ_B    = ast.NewObj(ast.Var, "b")
-	TEST_TREE          = &ast.File{
-		Name: &ast.Ident{
-			Name: "main",
+	OBJ_FUNC  = ast.NewObj(ast.Fun, "Addition")
+	OBJ_A     = ast.NewObj(ast.Var, "a")
+	OBJ_B     = ast.NewObj(ast.Var, "b")
+	FUNC_NAME = &ast.Ident{Name: "Addition", Obj: OBJ_FUNC}
+	FUNC_TYPE = &ast.FuncType{
+		Params: &ast.FieldList{List: []*ast.Field{{
+			Names: []*ast.Ident{{Name: "a", Obj: OBJ_A}, {Name: "b", Obj: OBJ_B}},
+			Type:  &ast.Ident{Name: "int"},
+		}}},
+		Results: &ast.FieldList{
+			Opening: token.NoPos,
+			List:    []*ast.Field{{Type: &ast.Ident{Name: "int"}}},
+			Closing: token.NoPos,
 		},
-		Decls: []ast.Decl{
-			0: &ast.FuncDecl{
-				Name: &ast.Ident{
-					Name: "Addition",
-					Obj:  TEST_TREE_OBJ_FUNC,
-				},
-				Type: &ast.FuncType{
-					Params: &ast.FieldList{
-						List: []*ast.Field{
-							0: {
-								Names: []*ast.Ident{
-									0: {
-										Name: "a",
-										Obj:  TEST_TREE_OBJ_A,
-									},
-									1: {
-										Name: "b",
-										Obj:  TEST_TREE_OBJ_B,
-									},
-								},
-								Type: &ast.Ident{
-									Name: "int",
-								},
-							},
-						},
-					},
-					Results: &ast.FieldList{
-						Opening: token.NoPos,
-						List: []*ast.Field{
-							0: {
-								Type: &ast.Ident{
-									Name: "int",
-								},
-							},
-						},
-						Closing: token.NoPos,
-					},
-				},
-				Body: &ast.BlockStmt{
-					List: []ast.Stmt{
-						0: &ast.ReturnStmt{
-							Results: []ast.Expr{
-								0: &ast.BinaryExpr{
-									X: &ast.Ident{
-										Name: "a",
-										Obj:  TEST_TREE_OBJ_A,
-									},
-									Op: token.ADD,
-									Y: &ast.Ident{
-										Name: "b",
-										Obj:  TEST_TREE_OBJ_B,
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-		Scope: &ast.Scope{
-			Objects: map[string]*ast.Object{
-				"Addition": TEST_TREE_OBJ_FUNC,
-			},
-		},
+	}
+	FUNC_BODY = &ast.BlockStmt{
+		List: []ast.Stmt{&ast.ReturnStmt{Results: []ast.Expr{&ast.BinaryExpr{
+			X:  &ast.Ident{Name: "a", Obj: OBJ_A},
+			Op: token.ADD,
+			Y:  &ast.Ident{Name: "b", Obj: OBJ_B},
+		}}}},
+	}
+	FUNC_DECL = &ast.FuncDecl{Name: FUNC_NAME, Type: FUNC_TYPE, Body: FUNC_BODY}
+	FILE      = &ast.File{
+		Name:       &ast.Ident{Name: "main"},
+		Decls:      []ast.Decl{FUNC_DECL},
+		Scope:      &ast.Scope{Objects: map[string]*ast.Object{"Addition": OBJ_FUNC}},
 		Unresolved: []*ast.Ident{},
 	}
 )
 
-func TestChildren(t *testing.T) {
-	compareSlices := func(a, b []ast.Node) bool {
-		if len(a) != len(b) {
-			return false
-		}
-		for i := 0; i < len(a); i++ {
-			if a[i] != b[i] {
-				return false
-			}
-		}
-		return true
+func compareSlices(a, b []ast.Node) error {
+	if len(a) != len(b) {
+		return fmt.Errorf("lengths mismatch a=%d b=%d", len(a), len(b))
 	}
-	testCases := []struct {
+	for i := range len(a) {
+		if a[i] != b[i] {
+			return fmt.Errorf("items mismatch: %d", i)
+		}
+	}
+	return nil
+}
+
+func TestChildren(t *testing.T) {
+	type tc struct {
 		input  ast.Node
 		output []ast.Node
-	}{{
-		input: TEST_TREE,
-		output: []ast.Node{
-			TEST_TREE.Name,
-			TEST_TREE.Decls[0].(*ast.FuncDecl),
+	}
+	testCases := map[string]tc{
+		"children of file": {
+			input:  FILE,
+			output: []ast.Node{FILE.Name, FUNC_DECL},
 		},
-	}, {
-		input: TEST_TREE.Decls[0],
-		output: []ast.Node{
-			TEST_TREE.Decls[0].(*ast.FuncDecl).Name,
-			TEST_TREE.Decls[0].(*ast.FuncDecl).Type,
-			TEST_TREE.Decls[0].(*ast.FuncDecl).Body,
+		"children of func decl": {
+			input:  FILE.Decls[0],
+			output: []ast.Node{FUNC_NAME, FUNC_TYPE, FUNC_BODY},
 		},
-	}}
-	for testIndex, testCase := range testCases {
-		got := children(testCase.input)
-		if !compareSlices(got, testCase.output) {
-			t.Errorf("Failed on comparison want == got for test #%d\n", testIndex)
-		}
+	}
+	for tn, tc := range testCases {
+		t.Run(tn, func(t *testing.T) {
+			got := children(tc.input)
+			if err := compareSlices(got, tc.output); err != nil {
+				t.Errorf("assert: %v", err)
+			}
+		})
 	}
 }
 
 func TestRecursively(t *testing.T) {
-	if Recursively(TEST_TREE, TEST_TREE) != true {
-		t.Error("Failed for same inputs")
-	}
-	if RecursivelyWithAddresses(TEST_TREE, TEST_TREE) != true {
-		t.Error("Failed for same inputs")
-	}
+	t.Run("same AST", func(t *testing.T) {
+		if !Recursively(FILE, FILE) {
+			t.Error("assertion, expected true")
+		}
+	})
+	t.Run("cloned AST", func(t *testing.T) {
+		if !Recursively(FILE, clone.File(FILE)) {
+			t.Error("assertion, expected true")
+		}
+	})
+}
 
-	TEST_TREE_NEW := clone.File(TEST_TREE)
-	if Recursively(TEST_TREE, TEST_TREE_NEW) != true {
-		t.Error("Failed for same inputs")
-	}
-	if RecursivelyWithAddresses(TEST_TREE, TEST_TREE_NEW) != false {
-		t.Error("Failed for same inputs")
-	}
-
-	TEST_TREE_NEW.Decls = append(TEST_TREE_NEW.Decls, &ast.GenDecl{})
-	if RecursivelyWithAddresses(TEST_TREE, TEST_TREE_NEW) != false {
-		t.Error("Failed for changed inputs")
-	}
+func TestRecursivelyWithAddresses(t *testing.T) {
+	t.Run("same AST", func(t *testing.T) {
+		if !RecursivelyWithAddresses(FILE, FILE) {
+			t.Error("assertion, expected true")
+		}
+	})
+	t.Run("cloned AST", func(t *testing.T) {
+		if RecursivelyWithAddresses(FILE, clone.File(FILE)) {
+			t.Error("assertion, expected false")
+		}
+	})
+	t.Run("cloned and edited AST", func(t *testing.T) {
+		cloned := clone.File(FILE)
+		cloned.Decls = append(cloned.Decls, &ast.GenDecl{})
+		if RecursivelyWithAddresses(FILE, cloned) {
+			t.Error("assertion, expected false")
+		}
+	})
 }
